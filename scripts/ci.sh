b570bef1e975ea194e01ee052220e646a8579997
Kobi Gurkan <kobigurk@gmail.com>
2019-05-09 13:14:22 +0300

    fix: attemps fixing ganache

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -xe
 
+echo "Working directory: `pwd`"
 ./compile.sh
 # ./do_setup.sh
 # ./build_verifier.sh
 npx mocha --recursive ../test
-./node_modules/.bin/ganache-cli -p 7545 -l 8800000 -i 5777
+../node_modules/.bin/ganache-cli -p 7545 -l 8800000 -i 5777
 truffle migrate --reset
