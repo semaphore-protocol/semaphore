59ee2c04a78ebba5b2e390e53feaec4cbce92508
Kobi Gurkan <kobigurk@gmail.com>
2019-05-09 13:14:22 +0300

    fix: adds setup and verifier builds

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,8 +2,8 @@
 
 echo "Working directory: `pwd`"
 ./compile.sh
-# ./do_setup.sh
-# ./build_verifier.sh
+./do_setup.sh
+./build_verifier.sh
 npx mocha --recursive ../test
 ../node_modules/.bin/ganache-cli -p 7545 -l 8800000 -i 5777 &
 ../node_modules/.bin/truffle migrate --reset
