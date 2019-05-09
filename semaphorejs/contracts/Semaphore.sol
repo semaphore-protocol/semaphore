0e672408881540a126d71d753c21fe453e4c9ad4
Kobi Gurkan <kobigurk@gmail.com>
2019-05-09 14:11:13 +0300

    feat: adds licensing and a bugfix in Semaphore.sol
    Former-commit-id: 299fb410593033311b02eecfbd0338ac57b8e0ee

--- semaphorejs/contracts/Semaphore.sol
+++ semaphorejs/contracts/Semaphore.sol
@@ -1,3 +1,23 @@
+/*
+ * semaphorejs - Zero-knowledge signaling on Ethereum
+ * Copyright (C) 2019 Kobi Gurkan <kobigurk@gmail.com>
+ *
+ * This file is part of semaphorejs.
+ *
+ * semaphorejs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semaphorejs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with semaphorejs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 pragma solidity >=0.4.21;
 
 import "./verifier.sol";
@@ -107,11 +127,11 @@ contract Semaphore is Verifier, MultipleMerkleTree {
       index = signal_tree_index;
     }
 
-    function setExternalNullifier(uint256 new_external_nullifier) {
+    function setExternalNullifier(uint256 new_external_nullifier) public onlyOwner {
       external_nullifier = new_external_nullifier;
     }
 
-    function setMaxGasPrice(uint256 new_max_gas_price) {
+    function setMaxGasPrice(uint256 new_max_gas_price) public onlyOwner {
       gas_price_max = new_max_gas_price;
     }
 }
--- semaphorejs/src/server/server.js
+++ semaphorejs/src/server/server.js
@@ -1,5 +1,25 @@
 #!/usr/bin/env node
 
+/*
+ * semaphorejs - Zero-knowledge signaling on Ethereum
+ * Copyright (C) 2019 Kobi Gurkan <kobigurk@gmail.com>
+ *
+ * This file is part of semaphorejs.
+ *
+ * semaphorejs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semaphorejs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with semaphorejs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const RocksDb = require('sbmtjs/src/storage/rocksdb');
 const MerkleTree = require('sbmtjs/src/tree');
 const Mimc7Hasher = require('sbmtjs/src/hasher/mimc7');
