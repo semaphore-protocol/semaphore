68dd558f40373672c0da932ddb8dc76dcc949a0c
Kobi Gurkan <kobigurk@gmail.com>
2019-06-22 19:23:50 +0300

    fix: fixes webpack config to remove blake2

--- semaphorejs/src/web/webpack.config.js
+++ semaphorejs/src/web/webpack.config.js
@@ -34,7 +34,7 @@ const browserConfig = {
   resolve: {
   },
   mode: prod ? 'production' : 'development',
-  externals: /^(worker_threads,blake2.wasm)$/
+  externals: /^(worker_threads)$/
 };
 
 module.exports = browserConfig;
