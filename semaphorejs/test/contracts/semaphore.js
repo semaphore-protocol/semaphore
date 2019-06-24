70299933593d09d8886b48f52c9858eb71667692
Kobi Gurkan <kobigurk@gmail.com>
2019-06-24 15:40:26 +0300

    fix: attempts printing vk_proof in test

--- semaphorejs/test/contracts/semaphore.js
+++ semaphorejs/test/contracts/semaphore.js
@@ -149,7 +149,7 @@ contract('Semaphore', function () {
         const vk_proof = fs.readFileSync(path.join(__dirname,'../../build/proving_key.bin'));
         const witness_bin = proof_util.convertWitness(snarkjs.stringifyBigInts(w));
         const publicSignals = w.slice(1, circuit.nPubInputs + circuit.nOutputs+1);
-        logger.info(`vk_proof: ${vk_proof}`);
+        console.log(`vk_proof: ${vk_proof}`);
         const proof = await proof_util.prove(witness_bin.buffer, vk_proof.buffer);
         await semaphore.broadcastSignal(
             signal_to_contract,
