a7f7b3fdb94e308dbe1e2376abc668017899313f
Kobi Gurkan <kobigurk@gmail.com>
2019-06-25 13:09:34 +0300

    Revert "Fixes contract tests"

--- semaphorejs/test/contracts/merkle_tree.js
+++ semaphorejs/test/contracts/merkle_tree.js
@@ -23,65 +23,23 @@ const chai = require('chai');
 
 const assert = chai.assert;
 
-const fs = require('fs');
-const del = require('del');
-const path = require('path');
 
 const MerkleTree = artifacts.require('MerkleTree');
 const MerkleTreeTester = artifacts.require('MerkleTreeTester');
 
-const RocksDb = require('zkp-sbmtjs/src/storage/rocksdb');
-const MerkleTreeLib = require('zkp-sbmtjs/src/tree');
-const Mimc7Hasher = require('zkp-sbmtjs/src/hasher/mimc7');
-
-
-
 contract('MerkleTree', () => {
     it('tests empty tree', async function () {
-        const storage_path = '/tmp/rocksdb_semaphore_test';
-        if (fs.existsSync(storage_path)) {
-            del.sync(storage_path, { force: true });
-        }
-        const default_value = '4';
-        const storage = new RocksDb(storage_path);
-        const hasher = new Mimc7Hasher();
-        const prefix = 'semaphore';
-        const tree = new MerkleTreeLib(
-            prefix,
-            storage,
-            hasher,
-            2,
-            default_value,
-        );
-
         const merkletree = await MerkleTree.deployed();
         const root = await merkletree.root();
-        const tree_root = await tree.root();
-        assert.equal(root.toString(10), tree_root.toString());
+        const expectedRoot = new BN('18939887937998136589474390185940729490221012739549343669326862744498332959884', 10);
+        assert.equal(root.toString(10), expectedRoot.toString(10));
     });
 
     it('tests insert', async function () {
-        const storage_path = '/tmp/rocksdb_semaphore_test2';
-        if (fs.existsSync(storage_path)) {
-            del.sync(storage_path, { force: true });
-        }
-        const default_value = '4';
-        const storage = new RocksDb(storage_path);
-        const hasher = new Mimc7Hasher();
-        const prefix = 'semaphore';
-        const tree = new MerkleTreeLib(
-            prefix,
-            storage,
-            hasher,
-            2,
-            default_value,
-        );
-
         const merkletree = await MerkleTreeTester.deployed();
         await merkletree.insert_test('5');
-        tree.update(0, '5');
         const root = await merkletree.root();
-        const tree_root = await tree.root();
-        assert.equal(root.toString(10), tree_root.toString());
+        const expectedRoot = new BN('1406701323482358960880977978809894610234893785434821064978317475451517385316', 10);
+        assert.equal(root.toString(10), expectedRoot.toString(10));
     });
 });
