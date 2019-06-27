e64c814051eca14c088dba29dd28e33039b01ab1
Koh Wei Jie <contact@kohweijie.com>
2019-06-28 03:11:19 +0800

    fixed path in ci.sh to get truffle test to pick up OZ contracts

--- semaphorejs/scripts/ci.sh
+++ semaphorejs/scripts/ci.sh
@@ -24,9 +24,9 @@ echo "Working directory: `pwd`"
 ./do_setup.sh
 ./convert_to_wasm.sh
 ./build_verifier.sh
-
 npx mocha --recursive ../test/circuits
 ../node_modules/.bin/ganache-cli -p 7545 -l 8800000 -i 5777 --account='0x6738837df169e8d6ffc6e33a2947e58096d644fa4aa6d74358c8d9d57c12cd21,100000000000000000000000000' --account='0x6738837df169e8d6ffc6e33a2947e58096d644fa4aa6d74358c8d9d57c12cd21,10000000000000000000000000' -q &
 ../node_modules/.bin/truffle migrate --reset
-../node_modules/.bin/truffle test ../test/contracts/* --network=local
+cd ..
+./node_modules/.bin/truffle test ../test/contracts/* --network=local
 
