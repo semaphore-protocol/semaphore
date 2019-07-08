2be4599a5c8ab6a6b9663918c16b6f8b026ac5f6
Kobi Gurkan <kobigurk@gmail.com>
2019-07-08 11:39:12 +0300

    fix: writes checksum to file

--- semaphorejs/scripts/checksum_snarks.sh
+++ semaphorejs/scripts/checksum_snarks.sh
@@ -2,6 +2,6 @@
 
 cd ..
 mkdir -p build
-find snark -type f -exec md5sum {} \; | sort -k 2 | md5sum
+find snark -type f -exec md5sum {} \; | sort -k 2 | md5sum > ./build/.snark_checksum
 echo './build/.snark_checksum:'
 cat ./build/.snark_checksum
