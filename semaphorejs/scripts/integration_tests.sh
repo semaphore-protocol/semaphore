8e54f9b5458e884d506295fd1e9c55a3af47512b
Kobi Gurkan <kobigurk@gmail.com>
2019-07-31 21:10:10 +0300

    fix: adds another account for ci testing

--- semaphorejs/scripts/integration_tests.sh
+++ semaphorejs/scripts/integration_tests.sh
@@ -22,7 +22,7 @@
 echo "Working directory: `pwd`"
 
 npx mocha --recursive ../test/circuits
-../node_modules/.bin/ganache-cli -p 7545 -l 8800000 -i 5777 --account='0x6738837df169e8d6ffc6e33a2947e58096d644fa4aa6d74358c8d9d57c12cd21,100000000000000000000000000' --account='0x6738837df169e8d6ffc6e33a2947e58096d644fa4aa6d74358c8d9d57c12cd21,10000000000000000000000000' -q &
+../node_modules/.bin/ganache-cli -p 7545 -l 8800000 -i 5777 --account='0x6738837df169e8d6ffc6e33a2947e58096d644fa4aa6d74358c8d9d57c12cd21,100000000000000000000000000' --account='0x6738837df169e8d6ffc6e33a2947e58096d644fa4aa6d74358c8d9d57c12cd22,10000000000000000000000000' -q &
 ../node_modules/.bin/truffle migrate --reset --network=local
 ../node_modules/.bin/truffle test ../test/contracts/* --network=local
 
