589ade76d5475bc2ce4377526d3451444a0d8da1
Koh Wei Jie <weijiekoh@users.noreply.github.com>
2020-03-22 20:23:43 +0800

    Fixed wrong link to semaphore-base.circom in docs (#37)
    * fixed wrong link to semaphore-base.circom in docs
    
    * updated the docs build script to not delete CNAME; fixed some minor errors in the tutorial
    
    * added warning about compiling scrypt in node 12
    
    * added warning about lerna
    
    Former-commit-id: 1e34d21f989550bb10c1a356e045770dfd4b0a6d

--- docs_src/build.sh
+++ docs_src/build.sh
@@ -3,4 +3,12 @@
 cd "$(dirname "$0")"
 mkdir -p ../docs
 
+if [ -f ../docs/CNAME ]; then
+    mv ../docs/CNAME ./CNAME_bak
+fi
+
 mdbook build . --dest-dir ../docs/ 
+
+if [ -f ./CNAME ]; then
+    mv ./CNAME_bak ../docs/
+fi
