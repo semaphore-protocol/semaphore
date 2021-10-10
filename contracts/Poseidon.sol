c7ca8dc02db2a07b3f932a10d701114b1c11b5b2
Andrija Novakovic <akinovak@gmail.com>
2021-10-10 12:06:43 +0200

    version 2.0
    Former-commit-id: 261fdc5b4419d12a1bb839a4c554cbe0a31d47c1

---
+++ contracts/Poseidon.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+library PoseidonT3 {
+    function poseidon(uint256[2] memory input) public pure returns (uint256) {}
+}
+
+
+library PoseidonT6 {
+    function poseidon(uint256[5] memory input) public pure returns (uint256) {}
+}
