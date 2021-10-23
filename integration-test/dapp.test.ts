57be569adb42bdeaa6f0381a88f99269ae7637f0
Andrija Novakovic <andrijanovakovic@Andrijas-MacBook-Pro.local>
2021-10-23 21:14:20 +0200

    integration tests
    Former-commit-id: 30dde96a1158c49adb8131a63e1b01a4249048db

--- integration-test/dapp.test.ts
+++ integration-test/dapp.test.ts
@@ -1,12 +1,79 @@
-import { ethers } from "ethers";
+const { ethers } = require("hardhat");
 import * as fs from 'fs';
-const deployedSemaphore = JSON.parse(fs.readFileSync('./deployments/localhost/Semaphore.json', { encoding: 'utf-8' }));
-const provider = ethers.providers.getDefaultProvider('http://localhost:8545');
-const Semaphore = new ethers.Contract(deployedSemaphore.address, deployedSemaphore.abi, provider);
+import * as path from 'path';
+import { ZkIdentity, Identity  } from "@libsem/identity";
+import { Semaphore, MerkleProof, IProof, genExternalNullifier, genSignalHash, generateMerkleProof  } from "@libsem/protocols";
+
+let deployedSemaphore;
+let provider;
+let SemaphoreContract;
+let defaultExternalNullifier: string;
+let wallet;
+const identityCommitments: Array<bigint> = [];
+const ZERO_VALUE = BigInt(ethers.utils.solidityKeccak256(['bytes'], [ethers.utils.toUtf8Bytes('Semaphore')]))
 
 beforeAll(async () => {
-
+    const [signer1] = await ethers.getSigners();
+    const signerAddress = await signer1.getAddress();
+    deployedSemaphore = JSON.parse(fs.readFileSync('./deployments/localhost/Semaphore.json', { encoding: 'utf-8' }));
+    provider = ethers.providers.getDefaultProvider('http://localhost:8545');
+    wallet = new ethers.Wallet(signerAddress, provider);
+    SemaphoreContract = new ethers.Contract(deployedSemaphore.address, deployedSemaphore.abi, wallet);
+    defaultExternalNullifier = genExternalNullifier("voting_1");
+    const semaphoreOwner = await SemaphoreContract.owner();
+    console.log('Semaphore owner: ', semaphoreOwner);
+    console.log('Signer', signer1.address);
+    const isOwner = await SemaphoreContract.isOwner();
+    console.log('Is owner', isOwner);
 })
 
 test('Semaphore proof', async () => {
+    const identity: ZkIdentity = new ZkIdentity();
+    const identityCommitment: bigint = identity.genIdentityCommitment();
+
+    const signal = "0x111";
+    const nullifierHash = Semaphore.genNullifierHash(defaultExternalNullifier, identity.getNullifier(), 20)
+
+    const commitments = Object.assign([], identityCommitments)
+    commitments.push(identityCommitment)
+
+    const merkleProof: MerkleProof = generateMerkleProof(20, ZERO_VALUE, 5, commitments, identityCommitment)
+    const witness: any = Semaphore.genWitness(identity.getIdentity(), merkleProof, defaultExternalNullifier, signal)
+
+    const wasmFilePath: string = path.join("./zkeyFiles", "semaphore.wasm")
+    const finalZkeyPath: string = path.join("./zkeyFiles", "semaphore_final.zkey")
+
+    // const fullProof = await Semaphore.genProof(witness, wasmFilePath, finalZkeyPath)
+    // const solidityProof = Semaphore.packToSolidityProof(fullProof);
+
+    // const packedProof = await SemaphoreContract.packProof(
+    //     solidityProof.a, 
+    //     solidityProof.b, 
+    //     solidityProof.c,
+    // );
+
+    // console.log(fullProof);
+
+    // const preBroadcastCheck = await SemaphoreContract.preBroadcastCheck(
+    //     ethers.utils.hexlify(ethers.utils.toUtf8Bytes(signal)),
+    //     packedProof,
+    //     merkleProof.root,
+    //     nullifierHash,
+    //     genSignalHash(signal),
+    //     defaultExternalNullifier
+    // )
+
+    // console.log('Prebroadcast check:', preBroadcastCheck);
+    // await tx.wait();
+    // let res = null;
+    // res = await SemaphoreContract.broadcastSignal(
+    //   ethers.utils.hexlify(ethers.utils.toUtf8Bytes(signal)),
+    //   packedProof,
+    //   merkleProof.root,
+    //   nullifierHash,
+    //   defaultExternalNullifier
+    // )
+
+    // console.log(res);
+
 });
