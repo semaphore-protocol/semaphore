e23235b2dcd6f2850a955f3a8aab4a69167d8ad6
Vadim Konstantinov <vadim.konstantinov@gmail.com>
2021-11-12 18:06:24 +0300

    Integration test updated after n_levels removed

--- integration-test/integration.test.ts
+++ integration-test/integration.test.ts
@@ -1,10 +1,12 @@
-import { ethers } from "ethers";
-import { ZkIdentity } from "@libsem/identity";
-import { Semaphore, generateMerkleProof, genExternalNullifier, FullProof, MerkleProof } from "@libsem/protocols";
+import {expect} from "chai";
+import {ethers} from "ethers";
+import {ZkIdentity} from "@libsem/identity";
+import {FullProof, generateMerkleProof, genExternalNullifier, MerkleProof, Semaphore} from "@libsem/protocols";
+import {genNullifierHash} from "../test/helpers";
 import * as path from "path";
 import * as fs from "fs";
 
-const SemaphoreInfo = JSON.parse(fs.readFileSync("./deployments/localhost/Semaphore.json", { encoding: "utf-8"}));
+const SemaphoreInfo = JSON.parse(fs.readFileSync("./deployments/localhost/Semaphore.json", {encoding: "utf-8"}));
 const pk = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 const defaultExternalNullifier = genExternalNullifier('test-voting');
 const identityCommitments: Array<bigint> = [];
@@ -17,6 +19,11 @@ beforeAll(async () => {
     const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
     const signer = new ethers.Wallet(pk, provider);
 
+    if (!!ethers["provider"] && !!ethers["getSigners"]) {
+        const defaultSigner = await ethers["getSigners"]()[0];
+        await defaultSigner.sendTransaction({to: signer.address, value: BigInt(1e18)});
+    }
+
     SemaphoreContract = new ethers.Contract(SemaphoreInfo.address, SemaphoreInfo.abi, provider);
     SemaphoreContract = SemaphoreContract.connect(signer);
 })
@@ -25,35 +32,34 @@ test('Should create semaphore full proof', async () => {
     const identity = new ZkIdentity();
     const identityCommitment = identity.genIdentityCommitment();
 
-    const semaphoreRootBefore = await SemaphoreContract.root();
-
     let tx = await SemaphoreContract.insertIdentity(identityCommitment);
     await tx.wait();
 
     const signal = "0x111";
-    const nullifierHash = Semaphore.genNullifierHash(defaultExternalNullifier, identity.getNullifier(), 20);
+    const nullifierHash = genNullifierHash(defaultExternalNullifier, identity.getNullifier());
 
-    const commitments = Object.assign([], identityCommitments);
-    commitments.push(identityCommitment);
+    identityCommitments.push(identityCommitment);
 
-    const merkleProof: MerkleProof = generateMerkleProof(20, ZERO_VALUE, 5, commitments, identityCommitment)
+    const merkleProof: MerkleProof = generateMerkleProof(20, ZERO_VALUE, 5, identityCommitments, identityCommitment)
     const witness = Semaphore.genWitness(identity.getIdentity(), merkleProof, defaultExternalNullifier, signal)
 
     const fullProof: FullProof = await Semaphore.genProof(witness, wasmFilePath, finalZkeyPath)
     const solidityProof = Semaphore.packToSolidityProof(fullProof);
 
     const packedProof = await SemaphoreContract.packProof(
-      solidityProof.a, 
-      solidityProof.b, 
-      solidityProof.c,
+        solidityProof.a,
+        solidityProof.b,
+        solidityProof.c,
     );
 
-    let res = null;
-    res = await SemaphoreContract.broadcastSignal(
-      ethers.utils.hexlify(ethers.utils.toUtf8Bytes(signal)),
-      packedProof,
-      merkleProof.root,
-      nullifierHash,
-      defaultExternalNullifier
-    )
-}, 60000)
+    let res = await SemaphoreContract.broadcastSignal(
+        ethers.utils.hexlify(ethers.utils.toUtf8Bytes(signal)),
+        packedProof,
+        merkleProof.root,
+        nullifierHash,
+        defaultExternalNullifier
+    );
+    expect(res).to.not.equal(null);
+    return Promise.resolve();
+
+}, 60000);
