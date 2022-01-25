46fbe572b7ca247ffc4cbb7a86b6252c448c32f7
cedoor <me@cedoor.dev>
2022-01-25 20:15:32 +0100

    chore: add ts script to compile circuits

--- scripts/bump-solidity.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const solidityRegex = /pragma solidity \^\d+\.\d+\.\d+/
-
-process.chdir('./contracts');
-const content = fs.readFileSync("Verifier.sol", { encoding: 'utf-8' });
-const bumped = content.replace(solidityRegex, 'pragma solidity ^0.8.0');
-
-
-fs.writeFileSync("Verifier.sol", bumped);
