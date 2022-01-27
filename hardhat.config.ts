21b7ccabd3511c40f92833fbdf52d893dc02331c
cedoor <me@cedoor.dev>
2022-01-27 16:37:04 +0100

    chore: add gas-reporter hardhat plugin
    Former-commit-id: 38bf1f58eac52efe12507c3839ab7b3bff84597f

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,15 @@
 import "@nomiclabs/hardhat-ethers"
 import "@nomiclabs/hardhat-waffle"
 import "@typechain/hardhat"
+import { config as dotenvConfig } from "dotenv"
 import "hardhat-deploy"
+import "hardhat-gas-reporter"
 import { HardhatUserConfig } from "hardhat/config"
+import { resolve } from "path"
 import "solidity-coverage"
 import { config } from "./package.json"
+
+dotenvConfig({ path: resolve(__dirname, "./.env") })
 
 const hardhatConfig: HardhatUserConfig = {
   solidity: config.solidity,
@@ -25,6 +30,11 @@ const hardhatConfig: HardhatUserConfig = {
       url: "http://localhost:8545",
       allowUnlimitedContractSize: true
     }
+  },
+  gasReporter: {
+    currency: "USD",
+    enabled: process.env.REPORT_GAS === "true",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY
   },
   typechain: {
     outDir: config.paths.build.typechain,
