5abfed2b775179c6cc818985381ddfe4da3cdf79
cedoor <me@cedoor.dev>
2022-01-27 11:24:48 +0100

    style: fix syntax with prettier
    Former-commit-id: f86391ad65895ec40fba56160f86e4bff9aceb6e

--- deploy/Semaphore.js
+++ deploy/Semaphore.js
@@ -1,29 +1,29 @@
-const { genExternalNullifier } = require('../utils');
+const { genExternalNullifier } = require("../utils")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-    const { deploy } = deployments;
-    const { deployer } = await getNamedAccounts();
-    const depth = 20;
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
+  const depth = 20
 
-    const poseidonT3 = await deployments.get("PoseidonT3")
-    const poseidonT6 = await deployments.get("PoseidonT6")
+  const poseidonT3 = await deployments.get("PoseidonT3")
+  const poseidonT6 = await deployments.get("PoseidonT6")
 
-    const externalNullifier = genExternalNullifier('test-voting');
-    const semaphore = await deploy('Semaphore', {
-      from: deployer,
-      log: true,
-      args: [depth, externalNullifier],
-      libraries: {
-        PoseidonT3: poseidonT3.address,
-        PoseidonT6: poseidonT6.address
-      }
-    });
+  const externalNullifier = genExternalNullifier("test-voting")
+  const semaphore = await deploy("Semaphore", {
+    from: deployer,
+    log: true,
+    args: [depth, externalNullifier],
+    libraries: {
+      PoseidonT3: poseidonT3.address,
+      PoseidonT6: poseidonT6.address
+    }
+  })
 
-    await deploy('SemaphoreClient', {
-      from: deployer,
-      log: true,
-      args: [semaphore.address],
-    });
-};
-module.exports.tags = ['complete'];
-module.exports.dependencies = ['PoseidonT3', 'PoseidonT6'];
+  await deploy("SemaphoreClient", {
+    from: deployer,
+    log: true,
+    args: [semaphore.address]
+  })
+}
+module.exports.tags = ["complete"]
+module.exports.dependencies = ["PoseidonT3", "PoseidonT6"]
--- scripts/compile-circuits.ts
+++ scripts/compile-circuits.ts
@@ -9,66 +9,66 @@ import logger from "js-logger"
 logger.useDefaults()
 
 async function exec(command: string) {
-    const { stderr, stdout } = await util.promisify(_exec)(command)
+  const { stderr, stdout } = await util.promisify(_exec)(command)
 
-    if (stderr) {
-        throw new Error(stderr)
-    }
+  if (stderr) {
+    throw new Error(stderr)
+  }
 
-    logger.info(stdout)
+  logger.info(stdout)
 }
 
 async function main() {
-    const buildPath = config.paths.build.snark
-    const solidityVersion = config.solidity.version
+  const buildPath = config.paths.build.snark
+  const solidityVersion = config.solidity.version
 
-    if (!fs.existsSync(buildPath)) {
-        fs.mkdirSync(buildPath, { recursive: true })
-    }
+  if (!fs.existsSync(buildPath)) {
+    fs.mkdirSync(buildPath, { recursive: true })
+  }
 
-    if (!fs.existsSync(`${buildPath}/powersOfTau28_hez_final_14.ptau`)) {
-        const url = "https://hermez.s3-eu-west-1.amazonaws.com"
-        const fileName = "powersOfTau28_hez_final_14.ptau"
+  if (!fs.existsSync(`${buildPath}/powersOfTau28_hez_final_14.ptau`)) {
+    const url = "https://hermez.s3-eu-west-1.amazonaws.com"
+    const fileName = "powersOfTau28_hez_final_14.ptau"
 
-        await download(`${url}/${fileName}`, buildPath)
-    }
+    await download(`${url}/${fileName}`, buildPath)
+  }
 
-    await exec(`circom ./circuits/semaphore.circom --r1cs --wasm --sym -o ${buildPath}`)
+  await exec(`circom ./circuits/semaphore.circom --r1cs --wasm --sym -o ${buildPath}`)
 
-    await r1cs.info(`${buildPath}/semaphore.r1cs`, logger)
+  await r1cs.info(`${buildPath}/semaphore.r1cs`, logger)
 
-    await zKey.newZKey(
-        `${buildPath}/semaphore.r1cs`,
-        `${buildPath}/powersOfTau28_hez_final_14.ptau`,
-        `${buildPath}/semaphore_0000.zkey`,
-        logger
-    )
+  await zKey.newZKey(
+    `${buildPath}/semaphore.r1cs`,
+    `${buildPath}/powersOfTau28_hez_final_14.ptau`,
+    `${buildPath}/semaphore_0000.zkey`,
+    logger
+  )
 
-    await zKey.beacon(
-        `${buildPath}/semaphore_0000.zkey`,
-        `${buildPath}/semaphore_final.zkey`,
-        "Final Beacon",
-        "0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
-        10,
-        logger
-    )
+  await zKey.beacon(
+    `${buildPath}/semaphore_0000.zkey`,
+    `${buildPath}/semaphore_final.zkey`,
+    "Final Beacon",
+    "0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
+    10,
+    logger
+  )
 
-    const verificationKey = await zKey.exportVerificationKey(`${buildPath}/semaphore_final.zkey`, logger)
+  const verificationKey = await zKey.exportVerificationKey(`${buildPath}/semaphore_final.zkey`, logger)
 
-    let verifierCode = await zKey.exportSolidityVerifier(
-        `${buildPath}/semaphore_final.zkey`,
-        { groth16: fs.readFileSync("./node_modules/snarkjs/templates/verifier_groth16.sol.ejs", "utf8") },
-        logger
-    )
-    verifierCode = verifierCode.replace(/pragma solidity \^\d+\.\d+\.\d+/, `pragma solidity ^${solidityVersion}`)
+  let verifierCode = await zKey.exportSolidityVerifier(
+    `${buildPath}/semaphore_final.zkey`,
+    { groth16: fs.readFileSync("./node_modules/snarkjs/templates/verifier_groth16.sol.ejs", "utf8") },
+    logger
+  )
+  verifierCode = verifierCode.replace(/pragma solidity \^\d+\.\d+\.\d+/, `pragma solidity ^${solidityVersion}`)
 
-    fs.writeFileSync(`${buildPath}/verification_key.json`, JSON.stringify(verificationKey), "utf-8")
-    fs.writeFileSync(`${config.paths.contracts}/Verifier.sol`, verifierCode, "utf-8")
+  fs.writeFileSync(`${buildPath}/verification_key.json`, JSON.stringify(verificationKey), "utf-8")
+  fs.writeFileSync(`${config.paths.contracts}/Verifier.sol`, verifierCode, "utf-8")
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
