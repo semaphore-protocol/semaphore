252788f172c77c6737360e07e1ed91542e0b6a85
cedoor <me@cedoor.dev>
2022-01-27 12:06:11 +0100

    style: fix contract syntax with prettier

--- contracts/Poseidon.sol
+++ contracts/Poseidon.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.4;
 
 library PoseidonT3 {
-    function poseidon(uint256[2] memory input) public pure returns (uint256) {}
+  function poseidon(uint256[2] memory input) public pure returns (uint256) {}
 }
 
-
 library PoseidonT6 {
-    function poseidon(uint256[5] memory input) public pure returns (uint256) {}
+  function poseidon(uint256[5] memory input) public pure returns (uint256) {}
 }
