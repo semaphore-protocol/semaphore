f2833daee5e1984d117c92aa0fdab39820864c91
cedoor <me@cedoor.dev>
2022-01-27 13:34:26 +0100

    chore: add typechain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,11 +3,19 @@ import { HardhatUserConfig } from "hardhat/config"
 
 import "@nomiclabs/hardhat-waffle"
 import "@nomiclabs/hardhat-ethers"
+import "@typechain/hardhat"
 import "hardhat-deploy"
 
 const hardhatConfig: HardhatUserConfig = {
   solidity: config.solidity,
-  paths: config.paths,
+  paths: {
+    sources: config.paths.contracts,
+    tests: config.paths.tests,
+    cache: config.paths.cache,
+    artifacts: config.paths.build.contracts,
+    deploy: config.paths.deploy,
+    deployments: config.paths.deployments
+  },
   networks: {
     hardhat: {
       chainId: 1337,
@@ -17,6 +25,10 @@ const hardhatConfig: HardhatUserConfig = {
       url: "http://localhost:8545",
       allowUnlimitedContractSize: true
     }
+  },
+  typechain: {
+    outDir: config.paths.build.typechain,
+    target: "ethers-v5"
   },
   namedAccounts: {
     deployer: 0
