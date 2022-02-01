c21d0b6e6b4189927b56f24f780f43daa5aad89b
cedoor <me@cedoor.dev>
2022-02-01 15:25:50 +0100

    feat: modularize contracts
    Former-commit-id: 431cac261b3bf105d85355131f929590fd7e8ecc

---
+++ contracts/Hashes.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+library PoseidonT3 {
+  function poseidon(uint256[2] memory) public pure returns (uint256) {}
+}
+
+library PoseidonT6 {
+  function poseidon(uint256[5] memory) public pure returns (uint256) {}
+}
--- tasks/deploy-semaphore.ts
+++ tasks/deploy-semaphore.ts
@@ -5,35 +5,43 @@ import { task, types } from "hardhat/config"
 
 task("deploy:semaphore", "Deploy a Semaphore contract")
   .addOptionalParam<boolean>("logs", "Print the logs", true, types.boolean)
-  .addOptionalParam<number>("depth", "Print the logs", 20, types.int)
-  .addOptionalParam<string>("nullifier", "Print the logs", "test", types.string)
-  .setAction(async ({ logs, depth, nullifier }, { ethers }): Promise<Contract> => {
+  .setAction(async ({ logs }, { ethers }): Promise<Contract> => {
     const poseidonT3ABI = poseidonContract.generateABI(2)
     const poseidonT3Bytecode = poseidonContract.createCode(2)
-    const poseidonT6ABI = poseidonContract.generateABI(5)
-    const poseidonT6Bytecode = poseidonContract.createCode(5)
+    // const poseidonT6ABI = poseidonContract.generateABI(5)
+    // const poseidonT6Bytecode = poseidonContract.createCode(5)
 
     const [signer] = await ethers.getSigners()
 
     const PoseidonLibT3Factory = new ethers.ContractFactory(poseidonT3ABI, poseidonT3Bytecode, signer)
     const poseidonT3Lib = await PoseidonLibT3Factory.deploy()
-    const PoseidonLibT6Factory = new ethers.ContractFactory(poseidonT6ABI, poseidonT6Bytecode, signer)
-    const poseidonT6Lib = await PoseidonLibT6Factory.deploy()
+    // const PoseidonLibT6Factory = new ethers.ContractFactory(poseidonT6ABI, poseidonT6Bytecode, signer)
+    // const poseidonT6Lib = await PoseidonLibT6Factory.deploy()
 
     await poseidonT3Lib.deployed()
-    await poseidonT6Lib.deployed()
+    // await poseidonT6Lib.deployed()
 
     logs && console.log(`PoseidonT3 library has been deployed to: ${poseidonT3Lib.address}`)
-    logs && console.log(`PoseidonT6 library has been deployed to: ${poseidonT6Lib.address}`)
+    // logs && console.log(`PoseidonT6 library has been deployed to: ${poseidonT6Lib.address}`)
 
-    const ContractFactory = await ethers.getContractFactory("Semaphore", {
+    const IncrementalMerkleTreeLibFactory = await ethers.getContractFactory("IncrementalTree", {
       libraries: {
-        PoseidonT3: poseidonT3Lib.address,
-        PoseidonT6: poseidonT6Lib.address
+        Hash: poseidonT3Lib.address
+      }
+    })
+    const incrementalMerkleTreeLib = await IncrementalMerkleTreeLibFactory.deploy()
+
+    await incrementalMerkleTreeLib.deployed()
+
+    logs && console.log(`IncrementalMerkleTree library has been deployed to: ${incrementalMerkleTreeLib.address}`)
+
+    const ContractFactory = await ethers.getContractFactory("SemaphoreGroups", {
+      libraries: {
+        IncrementalMerkleTree: incrementalMerkleTreeLib.address
       }
     })
 
-    const contract = await ContractFactory.deploy(depth, genExternalNullifier(nullifier))
+    const contract = await ContractFactory.deploy()
 
     await contract.deployed()
 
