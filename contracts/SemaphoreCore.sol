fdd4cef3b1c1b8b8c6692c72947403052b291ebb
cedoor <me@cedoor.dev>
2022-02-03 21:37:22 +0100

    feat: add core function to check proof validity

--- contracts/SemaphoreCore.sol
+++ contracts/SemaphoreCore.sol
@@ -14,7 +14,7 @@ contract SemaphoreCore is ISemaphoreCore, Verifier {
   /// It is used to prevent double-signaling.
   mapping(uint256 => bool) internal nullifierHashes;
 
-  /// @dev Checks if a nullifier already exists and if the zero-knowledge Semaphore proof is valid.
+  /// @dev Checks if the proof is valid.
   /// @param _signal: Semaphore signal.
   /// @param _root: Root of the Merkle tree.
   /// @param _nullifierHash: Nullifier hash.
@@ -27,32 +27,55 @@ contract SemaphoreCore is ISemaphoreCore, Verifier {
     uint256 _externalNullifier,
     uint256[8] calldata _proof
   ) {
+    require(
+      _isValidProof(_signal, _root, _nullifierHash, _externalNullifier, _proof),
+      "SemaphoreCore: the proof is not valid"
+    );
+
+    _;
+  }
+
+  /// @dev Returns true if no nullifier already exists and if the zero-knowledge proof is valid.
+  /// Otherwise it returns false.
+  /// @param _signal: Semaphore signal.
+  /// @param _root: Root of the Merkle tree.
+  /// @param _nullifierHash: Nullifier hash.
+  /// @param _externalNullifier: External nullifier.
+  /// @param _proof: Zero-knowledge proof.
+  function _isValidProof(
+    bytes memory _signal,
+    uint256 _root,
+    uint256 _nullifierHash,
+    uint256 _externalNullifier,
+    uint256[8] calldata _proof
+  ) internal view returns (bool) {
     require(nullifierHashes[_nullifierHash] == false, "SemaphoreCore: you cannot use the same nullifier twice");
 
-    uint256 signalHash = hashSignal(_signal);
+    uint256 signalHash = _hashSignal(_signal);
 
     uint256[4] memory publicSignals = [_root, _nullifierHash, signalHash, _externalNullifier];
 
-    require(
+    return
       verifyProof(
         [_proof[0], _proof[1]],
         [[_proof[2], _proof[3]], [_proof[4], _proof[5]]],
         [_proof[6], _proof[7]],
         publicSignals
-      ),
-      "Semaphore: the proof is not valid"
-    );
+      );
+  }
 
-    _;
-
-    // Store the nullifier hash to prevent double-signaling.
+  /// @dev Stores the nullifier hash to prevent double-signaling.
+  /// Attention! Remember to call it when you verify a proof if you
+  /// need to prevent double-signaling.
+  /// @param _nullifierHash: Semaphore nullifier hash.
+  function _saveNullifierHash(uint256 _nullifierHash) internal {
     nullifierHashes[_nullifierHash] = true;
   }
 
   /// @dev Creates a keccak256 hash of the signal.
   /// @param _signal: Semaphore signal.
   /// @return Hash of the signal.
-  function hashSignal(bytes memory _signal) internal pure returns (uint256) {
+  function _hashSignal(bytes memory _signal) private pure returns (uint256) {
     return uint256(keccak256(_signal)) >> 8;
   }
 }
