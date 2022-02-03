f06a12535f23f73aaed2283536fbc508fb9890be
cedoor <me@cedoor.dev>
2022-02-03 15:39:27 +0100

    feat: create abstract contract for nullifiers
    Former-commit-id: 7b2b4fd0c9c783e6381d50093b43aaf15c0c41d9

---
+++ contracts/SemaphoreNullifiers.sol
@@ -0,0 +1,41 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+import {SNARK_SCALAR_FIELD} from "./SemaphoreConstants.sol";
+import "./interfaces/ISemaphoreNullifiers.sol";
+
+/// @title Semaphore nullifiers contract.
+/// @dev The following code allows you to add or remove external nullifier to be used in proofs.
+/// External nullifiers help to prevent double-signaling by the same user.
+abstract contract SemaphoreNullifiers is ISemaphoreNullifiers {
+  /// @dev The external nullifier helps to prevent double-signaling by the same user.
+  mapping(uint256 => bool) internal externalNullifiers;
+
+  /// @dev Adds a new external nullifier.
+  /// @param _externalNullifier: External Semaphore nullifier.
+  function _addExternalNullifier(uint256 _externalNullifier) internal virtual {
+    require(externalNullifiers[_externalNullifier], "SemaphoreNullifiers: the external nullifier already exists");
+    require(
+      _externalNullifier < SNARK_SCALAR_FIELD,
+      "SemaphoreNullifiers: external nullifier must be < SNARK_SCALAR_FIELD"
+    );
+
+    externalNullifiers[_externalNullifier] = true;
+
+    emit ExternalNullifierAdded(_externalNullifier);
+  }
+
+  /// @dev Removes an existing external nullifier.
+  /// @param _externalNullifier: External Semaphore nullifier.
+  function _removeExternalNullifier(uint256 _externalNullifier) internal virtual {
+    require(!externalNullifiers[_externalNullifier], "SemaphoreNullifiers: the external nullifier does not exists");
+    require(
+      _externalNullifier < SNARK_SCALAR_FIELD,
+      "SemaphoreNullifiers: external nullifier must be < SNARK_SCALAR_FIELD"
+    );
+
+    externalNullifiers[_externalNullifier] = false;
+
+    emit ExternalNullifierRemoved(_externalNullifier);
+  }
+}
---
+++ contracts/interfaces/ISemaphoreNullifiers.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+/// @title SemaphoreNullifiers interface.
+/// @dev Interface of a SemaphoreNullifiers contract.
+interface ISemaphoreNullifiers {
+  /// @dev Emitted when a external nullifier is added.
+  /// @param externalNullifier: External Semaphore nullifier.
+  event ExternalNullifierAdded(uint256 externalNullifier);
+
+  /// @dev Emitted when a external nullifier is removed.
+  /// @param externalNullifier: External Semaphore nullifier.
+  event ExternalNullifierRemoved(uint256 externalNullifier);
+}
