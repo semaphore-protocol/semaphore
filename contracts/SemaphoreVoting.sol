406424c0a33c91e5a55488ff9a6c6589f4e2666f
cedoor <me@cedoor.dev>
2022-02-04 20:26:27 +0100

    refactor: remove timestamps and add poll state
    Former-commit-id: 3d01e97c886c9e32cd69de4577c04247d94915fe

--- contracts/SemaphoreVoting.sol
+++ contracts/SemaphoreVoting.sol
@@ -6,46 +6,45 @@ import "./SemaphoreCore.sol";
 import "./SemaphoreGroups.sol";
 
 /// @title Semaphore voting contract.
-/// @dev The following code allows you to create ballots, add voters and allow them to vote anonymously.
+/// @dev The following code allows you to create polls, add voters and allow them to vote anonymously.
 contract SemaphoreVoting is ISemaphoreVoting, SemaphoreCore, SemaphoreGroups {
-  /// @dev Gets a ballot id and returns the ballot data.
-  mapping(uint256 => Ballot) private ballots;
+  /// @dev Gets a poll id and returns the poll data.
+  mapping(uint256 => Poll) private polls;
 
-  /// @dev Checks if the ballot coordinator is the transaction sender.
-  /// @param ballotId: Id of the ballot.
-  modifier onlyCoordinator(uint256 ballotId) {
-    require(ballots[ballotId].coordinator == _msgSender(), "SemaphoreVoting: caller is not the ballot coordinator");
+  /// @dev Checks if the poll coordinator is the transaction sender.
+  /// @param pollId: Id of the poll.
+  modifier onlyCoordinator(uint256 pollId) {
+    require(polls[pollId].coordinator == _msgSender(), "SemaphoreVoting: caller is not the poll coordinator");
     _;
   }
 
-  /// @dev See {ISemaphoreVoting-createBallot}.
-  function createBallot(
-    uint256 ballotId,
-    address coordinator,
-    uint256 startDate,
-    uint256 endDate
-  ) public override {
-    _createGroup(ballotId, 20);
+  /// @dev See {ISemaphoreVoting-createPoll}.
+  function createPoll(uint256 pollId, address coordinator) public override {
+    _createGroup(pollId, 20);
 
-    Ballot memory ballot;
+    Poll memory poll;
 
-    ballot.coordinator = coordinator;
-    ballot.startDate = startDate;
-    ballot.endDate = endDate;
+    poll.coordinator = coordinator;
 
-    ballots[ballotId] = ballot;
+    polls[pollId] = poll;
 
-    emit BallotCreated(ballotId, coordinator, startDate, endDate);
+    emit PollCreated(pollId, coordinator);
   }
 
   /// @dev See {ISemaphoreVoting-addVoter}.
-  function addVoter(uint256 ballotId, uint256 identityCommitment) public override onlyCoordinator(ballotId) {
-    require(
-      block.timestamp < ballots[ballotId].startDate,
-      "SemaphoreVoting: voters can be added before the start date"
-    );
+  function addVoter(uint256 pollId, uint256 identityCommitment) public override onlyCoordinator(pollId) {
+    require(polls[pollId].state == PollState.Created, "SemaphoreVoting: voters can only be added before voting");
 
-    _addMember(ballotId, identityCommitment);
+    _addMember(pollId, identityCommitment);
+  }
+
+  /// @dev See {ISemaphoreVoting-addVoter}.
+  function startPoll(uint256 pollId, uint256 encryptionKey) public override onlyCoordinator(pollId) {
+    require(polls[pollId].state == PollState.Created, "SemaphoreVoting: poll has already been started");
+
+    polls[pollId].state = PollState.Ongoing;
+
+    emit PollStarted(pollId, _msgSender(), encryptionKey);
   }
 
   /// @dev See {ISemaphoreVoting-castVote}.
@@ -53,30 +52,31 @@ contract SemaphoreVoting is ISemaphoreVoting, SemaphoreCore, SemaphoreGroups {
     bytes calldata vote,
     uint256 root,
     uint256 nullifierHash,
-    uint256 ballotId,
+    uint256 pollId,
     uint256[8] calldata proof
-  ) public override onlyCoordinator(ballotId) {
-    Ballot memory ballot = ballots[ballotId];
+  ) public override onlyCoordinator(pollId) {
+    Poll memory poll = polls[pollId];
 
-    require(block.timestamp > ballot.startDate, "SemaphoreVoting: Invalid vote in advance");
-    require(block.timestamp < ballot.endDate, "SemaphoreVoting: Invalid late vote");
+    require(poll.state == PollState.Ongoing, "SemaphoreVoting: vote can only be cast in an ongoing poll");
 
-    require(ballotId == rootHistory[root], "SemaphoreVoting: the root does not match any ballot");
-    require(_isValidProof(vote, root, nullifierHash, ballotId, proof), "SemaphoreVoting: the proof is not valid");
+    require(pollId == rootHistory[root], "SemaphoreVoting: the root does not match any poll");
+    require(_isValidProof(vote, root, nullifierHash, pollId, proof), "SemaphoreVoting: the proof is not valid");
 
-    // Prevent double-voting (nullifierHash = hash(ballotId + identityNullifier)).
+    // Prevent double-voting (nullifierHash = hash(pollId + identityNullifier)).
     _saveNullifierHash(nullifierHash);
 
-    emit VoteAdded(ballotId, vote);
+    emit VoteAdded(pollId, vote);
   }
 
   /// @dev See {ISemaphoreVoting-publishDecryptionKey}.
-  function publishDecryptionKey(uint256 ballotId, uint256 decryptionKey) public override onlyCoordinator(ballotId) {
+  function endPoll(uint256 pollId, uint256 decryptionKey) public override onlyCoordinator(pollId) {
     require(
-      block.timestamp > ballots[ballotId].endDate,
-      "SemaphoreVoting: decryption key can be published after the end date"
+      polls[pollId].state == PollState.Ended,
+      "SemaphoreVoting: decryption key can only be published after voting"
     );
 
-    emit DecryptionKeyPublished(ballotId, decryptionKey);
+    polls[pollId].state = PollState.Ended;
+
+    emit PollEnded(pollId, _msgSender(), decryptionKey);
   }
 }
