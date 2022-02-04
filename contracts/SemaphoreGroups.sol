6d51e94a88d4695660ad86f54781b01375d55e2a
cedoor <me@cedoor.dev>
2022-02-04 12:52:30 +0100

    refactor: udpate method names
    Former-commit-id: e1189f316de36dc9e3a765ad45252c2ccc13631c

--- contracts/SemaphoreGroups.sol
+++ contracts/SemaphoreGroups.sol
@@ -40,7 +40,7 @@ abstract contract SemaphoreGroups is Context, ISemaphoreGroups {
   /// @dev Adds an identity commitment to an existing group.
   /// @param groupId: Id of the group.
   /// @param identityCommitment: New identity commitment.
-  function _addIdentityCommitment(uint256 groupId, uint256 identityCommitment) internal virtual {
+  function _addMember(uint256 groupId, uint256 identityCommitment) internal virtual {
     require(getDepth(groupId) != 0, "SemaphoreGroups: group does not exist");
     require(
       identityCommitment < SNARK_SCALAR_FIELD,
@@ -52,7 +52,7 @@ abstract contract SemaphoreGroups is Context, ISemaphoreGroups {
     uint256 root = getRoot(groupId);
     rootHistory[root] = groupId;
 
-    emit IdentityCommitmentAdded(groupId, identityCommitment, root);
+    emit MemberAdded(groupId, identityCommitment, root);
   }
 
   /// @dev Deletes an identity commitment from an existing group. A proof of membership is
@@ -61,7 +61,7 @@ abstract contract SemaphoreGroups is Context, ISemaphoreGroups {
   /// @param identityCommitment: Existing identity commitment to be deleted.
   /// @param proofSiblings: Array of the sibling nodes of the proof of membership.
   /// @param proofPathIndices: Path of the proof of membership.
-  function _deleteIdentityCommitment(
+  function _deleteMember(
     uint256 groupId,
     uint256 identityCommitment,
     uint256[4][] calldata proofSiblings,
@@ -78,21 +78,21 @@ abstract contract SemaphoreGroups is Context, ISemaphoreGroups {
     uint256 root = getRoot(groupId);
     rootHistory[root] = groupId;
 
-    emit IdentityCommitmentDeleted(groupId, identityCommitment, groups[groupId].root);
+    emit MemberDeleted(groupId, identityCommitment, groups[groupId].root);
   }
 
   /// @dev See {ISemaphoreGroups-getRoot}.
-  function getRoot(uint256 groupId) public view override returns (uint256) {
+  function getRoot(uint256 groupId) public view override virtual returns (uint256) {
     return groups[groupId].root;
   }
 
   /// @dev See {ISemaphoreGroups-getDepth}.
-  function getDepth(uint256 groupId) public view override returns (uint256) {
+  function getDepth(uint256 groupId) public view override virtual returns (uint256) {
     return groups[groupId].depth;
   }
 
   /// @dev See {ISemaphoreGroups-getSize}.
-  function getSize(uint256 groupId) public view override returns (uint256) {
+  function getSize(uint256 groupId) public view override virtual returns (uint256) {
     return groups[groupId].numberOfLeaves;
   }
 }
