6d313e194ee5c2660aaa4facbe4a2181c2cb019d
cedoor <me@cedoor.dev>
2022-02-07 16:24:21 +0100

    feat: add whistleblowing extension contract
    Former-commit-id: cec6ef81b062ea1ceed238382ed776d0c58cf013

---
+++ contracts/interfaces/ISemaphoreWhistleblowing.sol
@@ -0,0 +1,38 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+/// @title SemaphoreWhistleblowing interface.
+/// @dev Interface of SemaphoreWhistleblowing contract.
+interface ISemaphoreWhistleblowing {
+  /// @dev Emitted when a new entity is created.
+  /// @param entityId: Id of the entity.
+  /// @param editor: Editor of the entity.
+  event EntityCreated(uint256 entityId, address indexed editor);
+
+  /// @dev Emitted when a whistleblower publish a new leak.
+  /// @param entityId: Id of the entity.
+  /// @param leak: News leak.
+  event LeakPublished(uint256 indexed entityId, string leak);
+
+  /// @dev Creates an entity and the associated Merkle tree/group.
+  /// @param entityId: Id of the entity.
+  /// @param editor: Editor of the entity.
+  function createEntity(uint256 entityId, address editor) external;
+
+  /// @dev Adds a whistleblower to an entity.
+  /// @param entityId: Id of the entity.
+  /// @param identityCommitment: Identity commitment of the group member.
+  function addWhistleblower(uint256 entityId, uint256 identityCommitment) external;
+
+  /// @dev Allows whistleblowers to publish leaks anonymously.
+  /// @param leak: News leak.
+  /// @param nullifierHash: Nullifier hash.
+  /// @param entityId: Id of the entity.
+  /// @param proof: Private zk-proof parameters.
+  function publishLeak(
+    string calldata leak,
+    uint256 nullifierHash,
+    uint256 entityId,
+    uint256[8] calldata proof
+  ) external;
+}
