e2e4201e456be0d6f6dea5fd6b0a3bb07ccda531
cedoor <me@cedoor.dev>
2022-02-07 18:21:58 +0100

    test: add util function to create merkle proofs

--- test/utils.ts
+++ test/utils.ts
@@ -1,3 +1,9 @@
+import { generateMerkleProof } from "@zk-kit/protocols"
 import { ethers } from "ethers"
 
-export const TreeZeroNode = BigInt(ethers.utils.solidityKeccak256(["bytes"], [ethers.utils.toUtf8Bytes("Semaphore")]))
+export const SnarkScalarField = BigInt("21888242871839275222246405745257275088548364400416034343698204186575808495617")
+export const TreeZeroNode = BigInt(ethers.utils.solidityKeccak256(["string"], ["Semaphore"])) % SnarkScalarField
+
+export function createMerkleProof(leaves: bigint[], leaf: bigint) {
+  return generateMerkleProof(20, TreeZeroNode, 5, leaves, leaf)
+}
