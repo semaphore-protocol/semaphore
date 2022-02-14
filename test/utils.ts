286bb4a7b10cfb649fd1c63c9e68fded691c3f5a
cedoor <me@cedoor.dev>
2022-02-14 13:59:35 +0100

    chore: update @zk-kit/protocols pkg

--- test/utils.ts
+++ test/utils.ts
@@ -4,6 +4,6 @@ import { ethers } from "ethers"
 export const SnarkScalarField = BigInt("21888242871839275222246405745257275088548364400416034343698204186575808495617")
 export const TreeZeroNode = BigInt(ethers.utils.solidityKeccak256(["string"], ["Semaphore"])) % SnarkScalarField
 
-export function createMerkleProof(leaves: bigint[], leafIndex: number) {
-  return generateMerkleProof(20, TreeZeroNode, 5, leaves, leafIndex)
+export function createMerkleProof(leaves: bigint[], leaf: bigint) {
+  return generateMerkleProof(20, TreeZeroNode, 5, leaves, leaf)
 }
