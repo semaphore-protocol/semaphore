7d88065862fa289ef41de62e04b82c30038cb186
cedoor <me@cedoor.dev>
2022-02-18 15:45:13 +0100

    chore: add kovan & arbitrum networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import "@typechain/hardhat"
 import { config as dotenvConfig } from "dotenv"
 import "hardhat-gas-reporter"
 import { HardhatUserConfig } from "hardhat/config"
+import { NetworksUserConfig } from "hardhat/types"
 import { resolve } from "path"
 import "solidity-coverage"
 import { config } from "./package.json"
@@ -12,6 +13,26 @@ import "./tasks/deploy-semaphore-whistleblowing"
 import "./tasks/deploy-verifier"
 
 dotenvConfig({ path: resolve(__dirname, "./.env") })
+
+function getNetworks(): NetworksUserConfig | undefined {
+  if (process.env.INFURA_API_KEY && process.env.BACKEND_PRIVATE_KEY) {
+    const infuraApiKey = process.env.INFURA_API_KEY
+    const accounts = [`0x${process.env.BACKEND_PRIVATE_KEY}`]
+
+    return {
+      kovan: {
+        url: `https://kovan.infura.io/v3/${infuraApiKey}`,
+        chainId: 42,
+        accounts
+      },
+      arbitrum: {
+        url: "https://arb1.arbitrum.io/rpc",
+        chainId: 42161,
+        accounts
+      }
+    }
+  }
+}
 
 const hardhatConfig: HardhatUserConfig = {
   solidity: config.solidity,
@@ -26,10 +47,7 @@ const hardhatConfig: HardhatUserConfig = {
       chainId: 1337,
       allowUnlimitedContractSize: true
     },
-    localhost: {
-      url: "http://localhost:8545",
-      allowUnlimitedContractSize: true
-    }
+    ...getNetworks()
   },
   gasReporter: {
     currency: "USD",
