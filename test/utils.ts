b5389fed3d34b3940b343e765b092dd67d841246
cedoor <me@cedoor.dev>
2022-02-27 19:37:40 +0100

    chore: update @zk-kit/protocols pkg
    Former-commit-id: 4aced8e73e55cf4123863c99ff11434265bb65f3

--- test/utils.ts
+++ test/utils.ts
@@ -3,5 +3,5 @@ import { generateMerkleProof } from "@zk-kit/protocols"
 export const SnarkScalarField = BigInt("21888242871839275222246405745257275088548364400416034343698204186575808495617")
 
 export function createMerkleProof(leaves: bigint[], leaf: bigint) {
-  return generateMerkleProof(20, BigInt(0), 2, leaves, leaf)
+  return generateMerkleProof(20, BigInt(0), leaves, leaf)
 }
