528ad6ef4e88238f02b3f0df29bca80af1e51e54
Remco Bloemen <remco@wicked.ventures>
2022-03-23 15:05:31 -0700

    Remove unused code
    Former-commit-id: 475165fb774a3fc3da186006baab54948b88d095

--- contracts/base/Verifier.sol
+++ contracts/base/Verifier.sol
@@ -27,6 +27,7 @@ library Pairing {
     uint256 X;
     uint256 Y;
   }
+  
   // Encoding of field elements is: X[0] * z + X[1]
   struct G2Point {
     uint256[2] X;
@@ -40,7 +41,6 @@ library Pairing {
 
   /// @return the generator of G2
   function P2() internal pure returns (G2Point memory) {
-    // Original code point
     return
       G2Point(
         [
@@ -52,16 +52,6 @@ library Pairing {
           8495653923123431417604973247489272438418190587263600148770280649306958101930
         ]
       );
-
-    /*
-        // Changed by Jordi point
-        return G2Point(
-            [10857046999023057135944570762232829481370756359578518086990519993285655852781,
-             11559732032986387107991004021392285783925812861821192530917403151452391805634],
-            [8495653923123431417604973247489272438418190587263600148770280649306958101930,
-             4082367875863433681332203403145435568316851327593401208105741076214120093531]
-        );
-*/
   }
 
   /// @return r the negation of p, i.e. p.addition(p.negate()) should be zero.
