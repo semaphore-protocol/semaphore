b7b6e452d8512519413942d6d5bc2c1ee8b6bdad
cedoor <me@cedoor.dev>
2022-07-26 00:20:00 +0200

    chore: update snark artifacts url
    Former-commit-id: 45b8e95a459c3e6ea024ab391835280b029b80df

--- scripts/download-snark-artifacts.ts
+++ scripts/download-snark-artifacts.ts
@@ -4,7 +4,7 @@ import { config } from "../package.json"
 
 async function main() {
     const snarkArtifactsPath = config.paths.build["snark-artifacts"]
-    const url = `http://www.trusted-setup-pse.org/semaphore/${process.env.TREE_DEPTH}`
+    const url = `https://www.trusted-setup-pse.org/semaphore/${process.env.TREE_DEPTH}`
 
     if (!fs.existsSync(snarkArtifactsPath)) {
         fs.mkdirSync(snarkArtifactsPath, { recursive: true })
