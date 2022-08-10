86a6c2086ee2ba08e55c4e0399937e6db156cc42
semaraugusto <semaraugusto@dcc.ufmg.br>
2022-08-10 12:44:46 -0300

    fix CI issues (I think)

--- scripts/download-snark-artifacts.ts
+++ scripts/download-snark-artifacts.ts
@@ -3,16 +3,29 @@ import fs from "fs"
 import { config } from "../package.json"
 
 async function main() {
-    const snarkArtifactsPath = config.paths.build["snark-artifacts"]
-    const url = `https://www.trusted-setup-pse.org/semaphore/${process.env.TREE_DEPTH}`
+    let treeDepth: number;
+    let circuitLength: number;
+    if(process.env.TREE_DEPTH) {
+        treeDepth = Number(process.env.TREE_DEPTH)
+    } else {
+        treeDepth = 20
+    }
+    if(process.env.CIRCUIT_LENGTH) {
+        circuitLength = Number(process.env.CIRCUIT_LENGTH)
+    } else {
+        circuitLength = 2
+    }
+    const snarkArtifactsPath = `${config.paths.build["snark-artifacts"]}/${treeDepth}/${circuitLength}`
+    const url = `https://semaphore-fixtures.s3.amazonaws.com/${treeDepth}/${circuitLength}`
 
     if (!fs.existsSync(snarkArtifactsPath)) {
         fs.mkdirSync(snarkArtifactsPath, { recursive: true })
     }
 
     if (!fs.existsSync(`${snarkArtifactsPath}/semaphore.zkey`)) {
-        await download(`${url}/semaphore.wasm`, snarkArtifactsPath)
-        await download(`${url}/semaphore.zkey`, snarkArtifactsPath)
+        // await download(`https://semaphore-fixtures.s3.amazonaws.com/20/2/semaphore_20_2.wasm`)
+        await download(`${url}/semaphore_${treeDepth}_${circuitLength}.wasm`, snarkArtifactsPath)
+        await download(`${url}/circuit_final.zkey`, snarkArtifactsPath)
     }
 }
 
