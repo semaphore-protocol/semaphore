5fdf6c6547ea7c3a21d625a9f2afdd84b7796631
cedoor <me@cedoor.dev>
2022-08-18 13:36:10 +0200

    style: format code with prettier
    Former-commit-id: 4a2fddc5a0b2efd00c36fdfa75ea2f6e867902c5

--- contracts/extensions/SemaphoreWhistleblowing.sol
+++ contracts/extensions/SemaphoreWhistleblowing.sol
@@ -28,9 +28,9 @@ contract SemaphoreWhistleblowing is ISemaphoreWhistleblowing, SemaphoreCore, Sem
             "SemaphoreWhistleblowing: parameters lists does not have the same length"
         );
 
-        for (uint8 i = 0; i < depths.length;) {
+        for (uint8 i = 0; i < depths.length; ) {
             verifiers[depths[i]] = IVerifier(verifierAddresses[i]);
-            unchecked{
+            unchecked {
                 ++i;
             }
         }
