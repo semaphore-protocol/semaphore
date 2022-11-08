8ef22d1095ed12a9e62c9edd246f8e8421459a9a
taskooh <taskooh@github.com>
2022-11-08 22:01:25 +0900

    enable to build multiple circuits

--- scripts/build-snark-artifacts.sh
+++ scripts/build-snark-artifacts.sh
@@ -5,13 +5,11 @@ then
     exit
 fi
 
-artifacts_dir='snark-artifacts'
+build_circuits=( "semaphore" )
+artifacts_dir="snark-artifacts"
 
 # cleanup
 mkdir -p $artifacts_dir
-
-# TODO: Add artifacts
-circom packages/circuits/semaphore.circom --r1cs --wasm -o $artifacts_dir
 
 if [ ! -f $artifacts_dir/dev_final.ptau ]
 then
@@ -21,7 +19,11 @@ else
     echo "skip powers of tau"
 fi
 
-node node_modules/snarkjs/cli.js groth16 setup $artifacts_dir/semaphore.r1cs $artifacts_dir/dev_final.ptau $artifacts_dir/semaphore.zkey
-node node_modules/snarkjs/cli.js zkey export verificationkey $artifacts_dir/semaphore.zkey $artifacts_dir/semaphore.json
+for circuit in "${build_circuits[@]}"
+do
+    circom packages/circuits/$circuit.circom --r1cs --wasm -o $artifacts_dir
+    node node_modules/snarkjs/cli.js groth16 setup $artifacts_dir/$circuit.r1cs $artifacts_dir/dev_final.ptau $artifacts_dir/$circuit.zkey
+    node node_modules/snarkjs/cli.js zkey export verificationkey $artifacts_dir/$circuit.zkey $artifacts_dir/$circuit.json
+    mv $artifacts_dir/${circuit}_js/$circuit.wasm $artifacts_dir/$circuit.wasm
+done
 
-mv $artifacts_dir/semaphore_js/semaphore.wasm $artifacts_dir/semaphore.wasm
