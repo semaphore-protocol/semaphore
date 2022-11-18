bc14210bc7aa372837612760db35d2896e272b43
cedoor <me@cedoor.dev>
2022-11-18 16:13:25 +0100

    chore: add env var to download all snark artifacts
    Former-commit-id: 8f269ecafcd8a79733e2f4b3e85c2897bad6b3df

--- scripts/download-snark-artifacts.ts
+++ scripts/download-snark-artifacts.ts
@@ -1,15 +1,20 @@
 import download from "download"
-import fs from "fs"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 async function main() {
     const snarkArtifactsPath = "./snark-artifacts"
-    const url = `https://www.trusted-setup-pse.org/semaphore/${process.env.TREE_DEPTH || 20}`
 
-    if (!fs.existsSync(snarkArtifactsPath)) {
-        fs.mkdirSync(snarkArtifactsPath, { recursive: true })
-    }
+    if (process.env.ALL_SNARK_ARTIFACTS === "true") {
+        const url = `https://www.trusted-setup-pse.org/semaphore/semaphore.zip`
 
-    if (!fs.existsSync(`${snarkArtifactsPath}/semaphore.zkey`)) {
+        await download(url, snarkArtifactsPath, {
+            extract: true
+        })
+    } else {
+        const url = `https://www.trusted-setup-pse.org/semaphore/${process.env.TREE_DEPTH || 20}`
+
         await download(`${url}/semaphore.wasm`, snarkArtifactsPath)
         await download(`${url}/semaphore.zkey`, snarkArtifactsPath)
         await download(`${url}/semaphore.json`, snarkArtifactsPath)
