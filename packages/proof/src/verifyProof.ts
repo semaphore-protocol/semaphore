d880925604dacd4151cda6d924175f3fb825a1ad
cedoor <me@cedoor.dev>
2022-11-21 13:16:56 +0100

    refactor(proof): move zk verification keys to the code
    Former-commit-id: 26d1c5153e82875fc5ee6e003cdf1ac4bbf43833

--- packages/proof/src/verifyProof.ts
+++ packages/proof/src/verifyProof.ts
@@ -1,13 +1,24 @@
 import { groth16 } from "snarkjs"
 import { FullProof } from "./types"
+import verificationKeys from "./verificationKeys.json"
 
 /**
  * Verifies a SnarkJS proof.
- * @param verificationKey The zero-knowledge verification key.
  * @param fullProof The SnarkJS full proof.
+ * @param treeDepth The Merkle tree depth.
  * @returns True if the proof is valid, false otherwise.
  */
-export default function verifyProof(verificationKey: any, { proof, publicSignals }: FullProof): Promise<boolean> {
+export default function verifyProof({ proof, publicSignals }: FullProof, treeDepth: number): Promise<boolean> {
+    if (treeDepth < 16 || treeDepth > 32) {
+        throw new TypeError("The tree depth must be a number between 16 and 32")
+    }
+
+    const verificationKey = {
+        ...verificationKeys,
+        vk_delta_2: verificationKeys.vk_delta_2[treeDepth - 16],
+        IC: verificationKeys.IC[treeDepth - 16]
+    }
+
     return groth16.verify(
         verificationKey,
         [
