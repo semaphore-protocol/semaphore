3b2040247637a358414d5d6a7dc37a947d938d16
cedoor <me@cedoor.dev>
2023-01-04 13:20:54 +0100

    refactor(contracts): update function to verify contracts
    Former-commit-id: cd65440f2c48254fbb02973823339e6fbabb69d2

--- packages/contracts/scripts/verify-contracts.ts
+++ packages/contracts/scripts/verify-contracts.ts
@@ -1,24 +1,26 @@
 import { hardhatArguments, run } from "hardhat"
 import { getDeployedContracts } from "./utils"
 
+async function verify(address: string, constructorArguments?: any[]): Promise<void> {
+    try {
+        await run("verify:verify", {
+            address,
+            constructorArguments
+        })
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 async function main() {
     const deployedContracts = getDeployedContracts(hardhatArguments.network)
 
-    await run("verify:verify", {
-        address: deployedContracts.IncrementalBinaryTree
-    })
-
-    await run("verify:verify", {
-        address: deployedContracts.Semaphore
-    })
-
-    await run("verify:verify", {
-        address: deployedContracts.Pairing
-    })
-
-    await run("verify:verify", {
-        address: deployedContracts.SemaphoreVerifier
-    })
+    if (deployedContracts) {
+        await verify(deployedContracts.IncrementalBinaryTree)
+        await verify(deployedContracts.Pairing)
+        await verify(deployedContracts.SemaphoreVerifier)
+        await verify(deployedContracts.Semaphore, [deployedContracts.SemaphoreVerifier])
+    }
 }
 
 main()
