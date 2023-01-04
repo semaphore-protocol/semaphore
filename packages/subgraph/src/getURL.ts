32aabf81d274ad32385ad884d53fce81a1eb1aaf
cedoor <me@cedoor.dev>
2023-01-04 19:08:52 +0100

    fix(subgraph): set correct goerli subgraph url
    Former-commit-id: ce5c30b816f0ead9c9a229c6c698b90590c299e9

--- packages/subgraph/src/getURL.ts
+++ packages/subgraph/src/getURL.ts
@@ -8,7 +8,7 @@ import { Network } from "./types"
 export default function getURL(network: Network): string {
     switch (network) {
         case "goerli":
-            return `https://api.thegraph.com/subgraphs/name/semaphore-protocol/goerli-5259d3`
+            return `https://api.thegraph.com/subgraphs/name/semaphore-protocol/goerli-89490c`
         // case "arbitrum":
         // return `https://api.thegraph.com/subgraphs/name/semaphore-protocol/arbitrum-86337c`
         default:
