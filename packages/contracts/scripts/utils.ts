cd65440f2c48254fbb02973823339e6fbabb69d2
cedoor <me@cedoor.dev>
2023-01-04 13:20:54 +0100

    refactor(contracts): update function to verify contracts

--- packages/contracts/scripts/utils.ts
+++ packages/contracts/scripts/utils.ts
@@ -1,33 +1,21 @@
 import { readFileSync, writeFileSync } from "fs"
 
 type DeployedContracts = {
-    Pairing?: string
-    SemaphoreVerifier?: string
-    Poseidon?: string
-    IncrementalBinaryTree?: string
-    Semaphore?: string
+    Pairing: string
+    SemaphoreVerifier: string
+    Poseidon: string
+    IncrementalBinaryTree: string
+    Semaphore: string
 }
 
 export function getDeployedContracts(network: string | undefined): DeployedContracts | null {
     try {
         return JSON.parse(readFileSync(`./deployed-contracts/${network}.json`, "utf8"))
     } catch (error) {
-        return {}
+        return null
     }
 }
 
-export function saveDeployedContracts(network: string | undefined, newDeployedContracts: DeployedContracts) {
-    const deployedContracts = getDeployedContracts(network)
-
-    writeFileSync(
-        `./deployed-contracts/${network}.json`,
-        JSON.stringify(
-            {
-                ...deployedContracts,
-                ...newDeployedContracts
-            },
-            null,
-            4
-        )
-    )
+export function saveDeployedContracts(network: string | undefined, deployedContracts: DeployedContracts) {
+    writeFileSync(`./deployed-contracts/${network}.json`, JSON.stringify(deployedContracts, null, 4))
 }
