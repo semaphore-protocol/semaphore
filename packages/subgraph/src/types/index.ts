a679d047fa649a2c048349f0d86dbf02d1208a11
Saleel <saleel@me.com>
2023-02-15 21:39:11 +0530

    feat: add query filters to subgraph library
    Former-commit-id: bc8f3495b63d9ca6d98a55e46bec6ba1bac06e5c

--- packages/subgraph/src/types/index.ts
+++ packages/subgraph/src/types/index.ts
@@ -3,4 +3,10 @@ export type Network = "goerli" | "arbitrum"
 export type GroupOptions = {
     members?: boolean
     verifiedProofs?: boolean
+    filters?: {
+        admin?: string
+        timestamp?: Date
+        timestampGte?: Date
+        timestampLte?: Date
+    }
 }
