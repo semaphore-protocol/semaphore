00e3c4a7f5ab6ba765c998fb42a6bf27489ec5e0
cedoor <me@cedoor.dev>
2023-03-09 18:21:44 +0000

    feat(data): add support for optimism goerli network
    re #234
    
    
    Former-commit-id: 3722b154bf1a1ab1c2833b94ab23268ebdf245bd

--- packages/data/src/getURL.ts
+++ packages/data/src/getURL.ts
@@ -8,11 +8,10 @@ import { Network } from "./types"
 export default function getURL(network: Network): string {
     switch (network) {
         case "goerli":
-            return `https://api.studio.thegraph.com/query/14377/semaphore-goerli/v3.2.0`
         case "mumbai":
-            return `https://api.studio.thegraph.com/query/14377/semaphore-mumbai/v3.2.0`
+        case "optimism-goerli":
         case "arbitrum":
-            return `https://api.studio.thegraph.com/query/14377/semaphore-arbitrum/v3.2.0`
+            return `https://api.studio.thegraph.com/query/14377/semaphore-${network}/v3.2.0`
         default:
             throw new TypeError(`Network '${network}' is not supported`)
     }
