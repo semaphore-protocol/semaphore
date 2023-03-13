59269b067dbe018485ae1715836b62cb14250599
cedoor <me@cedoor.dev>
2023-03-13 22:32:23 +0000

    build: bundle poseidon-lite functions with rollup
    Former-commit-id: 797e62dd282fc5440b9e1ae944bf8461b1fa66b6

--- packages/identity/rollup.config.ts
+++ packages/identity/rollup.config.ts
@@ -1,6 +1,8 @@
 import typescript from "rollup-plugin-typescript2"
+import commonjs from "@rollup/plugin-commonjs"
 import * as fs from "fs"
 import cleanup from "rollup-plugin-cleanup"
+import { nodeResolve } from "@rollup/plugin-node-resolve"
 
 const pkg = JSON.parse(fs.readFileSync("./package.json", "utf-8"))
 const banner = `/**
@@ -24,6 +26,8 @@ export default {
             tsconfig: "./build.tsconfig.json",
             useTsconfigDeclarationDir: true
         }),
+        commonjs(),
+        nodeResolve(),
         cleanup({ comments: "jsdoc" })
     ]
 }
