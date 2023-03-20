4133ae12c31eb3683b8f20b0a9c4482dc8776691
vplasencia <v.pcalana@gmail.com>
2023-03-20 12:36:12 +0100

    feat(cli-template-hardhat): add new version of cli-template-hardhat

--- packages/cli-template-hardhat/hardhat.config.ts
+++ packages/cli-template-hardhat/hardhat.config.ts
@@ -1,50 +1,63 @@
-import "@nomicfoundation/hardhat-toolbox"
+import "@nomiclabs/hardhat-ethers"
+import "@nomicfoundation/hardhat-chai-matchers"
 import "@semaphore-protocol/hardhat"
+import "@typechain/hardhat"
 import { config as dotenvConfig } from "dotenv"
+import "hardhat-gas-reporter"
 import { HardhatUserConfig } from "hardhat/config"
 import { NetworksUserConfig } from "hardhat/types"
+import { resolve } from "path"
+import "solidity-coverage"
+import { config } from "./package.json"
 import "./tasks/deploy"
 
-dotenvConfig()
+dotenvConfig({ path: resolve(__dirname, "../../.env") })
 
 function getNetworks(): NetworksUserConfig {
-    if (process.env.ETHEREUM_URL && process.env.ETHEREUM_PRIVATE_KEY) {
-        const accounts = [`0x${process.env.ETHEREUM_PRIVATE_KEY}`]
-
-        return {
-            goerli: {
-                url: process.env.ETHEREUM_URL,
-                chainId: 5,
-                accounts
-            },
-            sepolia: {
-                url: process.env.ETHEREUM_URL,
-                chainId: 11155111,
-                accounts
-            },
-            mumbai: {
-                url: process.env.ETHEREUM_URL,
-                chainId: 80001,
-                accounts
-            },
-            "optimism-goerli": {
-                url: process.env.ETHEREUM_URL,
-                chainId: 420,
-                accounts
-            },
-            arbitrum: {
-                url: process.env.ETHEREUM_URL,
-                chainId: 42161,
-                accounts
-            }
-        }
+    if (!process.env.INFURA_API_KEY || !process.env.ETHEREUM_PRIVATE_KEY) {
+        return {}
     }
 
-    return {}
+    const accounts = [`0x${process.env.ETHEREUM_PRIVATE_KEY}`]
+    const infuraApiKey = process.env.INFURA_API_KEY
+
+    return {
+        goerli: {
+            url: `https://goerli.infura.io/v3/${infuraApiKey}`,
+            chainId: 5,
+            accounts
+        },
+        sepolia: {
+            url: `https://sepolia.infura.io/v3/${infuraApiKey}`,
+            chainId: 11155111,
+            accounts
+        },
+        mumbai: {
+            url: `https://polygon-mumbai.infura.io/v3/${infuraApiKey}`,
+            chainId: 80001,
+            accounts
+        },
+        "optimism-goerli": {
+            url: `https://optimism-goerli.infura.io/v3/${infuraApiKey}`,
+            chainId: 420,
+            accounts
+        },
+        arbitrum: {
+            url: `https://arbitrum-mainnet.infura.io/v3/${infuraApiKey}`,
+            chainId: 42161,
+            accounts
+        }
+    }
 }
 
-const config: HardhatUserConfig = {
-    solidity: "0.8.4",
+const hardhatConfig: HardhatUserConfig = {
+    solidity: config.solidity,
+    paths: {
+        sources: config.paths.contracts,
+        tests: config.paths.tests,
+        cache: config.paths.cache,
+        artifacts: config.paths.build.contracts
+    },
     networks: {
         hardhat: {
             chainId: 1337
@@ -55,7 +68,11 @@ const config: HardhatUserConfig = {
         currency: "USD",
         enabled: process.env.REPORT_GAS === "true",
         coinmarketcap: process.env.COINMARKETCAP_API_KEY
+    },
+    typechain: {
+        outDir: config.paths.build.typechain,
+        target: "ethers-v5"
     }
 }
 
-export default config
+export default hardhatConfig
