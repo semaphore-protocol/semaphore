b89ed0ebb76e08f1f54a7ffcb44ced9aad81947c
vplasencia <v.pcalana@gmail.com>
2023-04-03 18:25:57 +0200

    refactor(cli-template-hardhat-nextjs-semaphoresubgraph): remove unused import statement
    Former-commit-id: 5dbbdd1b2cc621a8849987e3d829c249c0aa1f30

--- packages/cli-template-hardhat-nextjs-semaphoresubgraph/apps/web-app/src/pages/_app.tsx
+++ packages/cli-template-hardhat-nextjs-semaphoresubgraph/apps/web-app/src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import "../styles/globals.css"
-import { Network } from "@semaphore-protocol/data"
 import type { AppProps } from "next/app"
 import Head from "next/head"
 import { useRouter } from "next/router"
