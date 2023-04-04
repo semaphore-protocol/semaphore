e4a260c4b837180178b9d1d324e147430764e07b
vplasencia <v.pcalana@gmail.com>
2023-04-04 09:06:03 +0200

    feat(cli): add inquirer to select a template when creating a project

--- packages/cli/src/inquirerPrompts.ts
+++ packages/cli/src/inquirerPrompts.ts
@@ -10,11 +10,22 @@ export async function getProjectName() {
     return projectName
 }
 
+export async function getSupportedTemplate(supportedTemplates: string[]) {
+    const { selectedTemplate } = await inquirer.prompt({
+        name: "selectedTemplate",
+        type: "list",
+        message: "Select one of the supported templates:",
+        default: supportedTemplates[0],
+        choices: supportedTemplates
+    })
+    return selectedTemplate
+}
+
 export async function getSupportedNetwork(supportedNetworks: string[]) {
     const { selectedNetwork } = await inquirer.prompt({
         name: "selectedNetwork",
         type: "list",
-        message: "Select one of our supported networks:",
+        message: "Select one of the supported networks:",
         default: supportedNetworks[0],
         choices: supportedNetworks
     })
