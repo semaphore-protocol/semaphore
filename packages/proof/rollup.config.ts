ba9782139e011b1c9aabc373c3fbc414718d06f6
cedoor <me@cedoor.dev>
2023-04-22 17:23:52 +0100

    feat(proof): add function to calculate nullifier hash
    Former-commit-id: 6cc2b6af1c4602e6026198a393960f4d6909389a

--- packages/proof/rollup.config.ts
+++ packages/proof/rollup.config.ts
@@ -1,7 +1,9 @@
 import typescript from "rollup-plugin-typescript2"
+import commonjs from "@rollup/plugin-commonjs"
 import * as fs from "fs"
 import cleanup from "rollup-plugin-cleanup"
 import json from "@rollup/plugin-json"
+import { nodeResolve } from "@rollup/plugin-node-resolve"
 
 const pkg = JSON.parse(fs.readFileSync("./package.json", "utf-8"))
 const banner = `/**
@@ -25,6 +27,8 @@ export default {
             tsconfig: "./build.tsconfig.json",
             useTsconfigDeclarationDir: true
         }),
+        commonjs(),
+        nodeResolve(),
         cleanup({ comments: "jsdoc" }),
         json()
     ]
---
+++ packages/proof/src/calculateNullifierHash.ts
@@ -0,0 +1,16 @@
+import { BytesLike, Hexable } from "@ethersproject/bytes"
+import { poseidon2 } from "poseidon-lite/poseidon2"
+import hash from "./hash"
+
+/**
+ * Given the identity nullifier and the external nullifier, it calculates nullifier hash.
+ * @param identityNullifier The identity nullifier.
+ * @param externalNullifier The external nullifier.
+ * @returns The nullifier hash.
+ */
+export default function calculateNullifierHash(
+    identityNullifier: number | bigint | string,
+    externalNullifier: BytesLike | Hexable | number | bigint
+): bigint {
+    return poseidon2([hash(externalNullifier), identityNullifier])
+}
