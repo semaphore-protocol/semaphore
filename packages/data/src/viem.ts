7146f3c3104db7976fe5bf9ad360fb2d0d706aee
Inaridiy <inainainariinainari@gmail.com>
2023-09-22 20:44:54 +0900

    feat(data): impl SemaphoreViem fetch methods

--- packages/data/src/viem.ts
+++ packages/data/src/viem.ts
@@ -1,14 +1,19 @@
-import { Chain, PublicClient, http, createPublicClient, getContract, GetContractReturnType } from "viem"
+import { Chain, PublicClient, http, createPublicClient, getContract, GetContractReturnType, Transport } from "viem"
 import { arbitrum, arbitrumGoerli, polygon, goerli, sepolia, optimismGoerli } from "viem/chains"
 import SemaphoreABI from "./semaphoreABI"
-import { ViemOptions } from "./types"
+import { GroupResponse, ViemOptions } from "./types"
 import checkParameter from "./checkParameter"
 
 export default class SemaphoreViem {
     private _client: PublicClient
     private _options: ViemOptions
-    private _contract: GetContractReturnType<typeof SemaphoreABI>
+    private _contract: GetContractReturnType<typeof SemaphoreABI, PublicClient>
 
+    /**
+     * Initializes the Viem object with an viem chain.
+     * @param network Chain object of viem.
+     * @param options Viem options.
+     */
     constructor(network: Chain, options: ViemOptions) {
         if (options.rpcURL && !options.rpcURL.startsWith("http")) {
             checkParameter(options.rpcURL, "networkOrSubgraphURL", "url string")
@@ -52,5 +57,137 @@ export default class SemaphoreViem {
         this._client = client
         this._options = options
         this._contract = getContract({ address: options.address, abi: SemaphoreABI, publicClient: client })
+    }
+
+    async getGroupIds(): Promise<string[]> {
+        const groups = await this._contract.getEvents.GroupCreated(
+            {},
+            { fromBlock: BigInt(this._options.startBlock!), toBlock: "latest" }
+        )
+        return groups.map((group) => String(group.args.groupId))
+    }
+
+    async getGroup(groupId: string): Promise<GroupResponse> {
+        checkParameter(groupId, "groupId", "string")
+
+        const _groupId = BigInt(groupId)
+        const [groupCreatedEvent] = await this._contract.getEvents.GroupCreated(
+            { groupId: _groupId },
+            { fromBlock: BigInt(this._options.startBlock!), toBlock: "latest" }
+        )
+
+        if (!groupCreatedEvent) {
+            throw new Error(`Group '${groupId}' not found`)
+        }
+
+        const merkleTreeRoot = await this._contract.read.getMerkleTreeRoot([_groupId])
+        const numberOfLeaves = await this._contract.read.getNumberOfMerkleTreeLeaves([_groupId])
+
+        const group: GroupResponse = {
+            id: groupId,
+            merkleTree: {
+                depth: Number(groupCreatedEvent.args.merkleTreeDepth),
+                zeroValue: groupCreatedEvent.args.zeroValue?.toString() as string,
+                numberOfLeaves: Number(numberOfLeaves),
+                root: merkleTreeRoot?.toString() as string
+            }
+        }
+
+        return group
+    }
+
+    async getGroupAdmin(groupId: string): Promise<string> {
+        checkParameter(groupId, "groupId", "string")
+
+        const groupAdminUpdatedEvents = await this._contract.getEvents.GroupAdminUpdated(
+            { groupId: BigInt(groupId) },
+            { fromBlock: BigInt(this._options.startBlock!), toBlock: "latest" }
+        )
+
+        if (groupAdminUpdatedEvents.length === 0) {
+            throw new Error(`Group '${groupId}' not found`)
+        }
+
+        return groupAdminUpdatedEvents[groupAdminUpdatedEvents.length - 1].args.newAdmin?.toString() as string
+    }
+
+    async getGroupMembers(groupId: string): Promise<string[]> {
+        checkParameter(groupId, "groupId", "string")
+
+        const _groupId = BigInt(groupId)
+        const [groupCreatedEvent] = await this._contract.getEvents.GroupCreated(
+            { groupId: _groupId },
+            { fromBlock: BigInt(this._options.startBlock!), toBlock: "latest" }
+        )
+
+        if (!groupCreatedEvent) {
+            throw new Error(`Group '${groupId}' not found`)
+        }
+
+        const zeroValue = groupCreatedEvent.args.zeroValue?.toString() as string
+        const memberRemovedEvents = await this._contract.getEvents.MemberRemoved(
+            { groupId: _groupId },
+            { fromBlock: BigInt(this._options.startBlock!), toBlock: "latest" }
+        )
+        const memberUpdatedEvents = await this._contract.getEvents.MemberUpdated(
+            { groupId: _groupId },
+            { fromBlock: BigInt(this._options.startBlock!), toBlock: "latest" }
+        )
+
+        const groupUpdates = new Map<string, [bigint, string]>()
+
+        for (const { blockNumber, args } of memberUpdatedEvents) {
+            groupUpdates.set(args.index?.toString()!, [blockNumber, args.newIdentityCommitment?.toString()!])
+        }
+
+        for (const { blockNumber, args } of memberRemovedEvents) {
+            const groupUpdate = groupUpdates.get(args.index?.toString()!)
+
+            if (!groupUpdate || (groupUpdate && groupUpdate[0] < blockNumber)) {
+                groupUpdates.set(args.index?.toString()!, [blockNumber, zeroValue])
+            }
+        }
+
+        const memberAddedEvents = await this._contract.getEvents.MemberAdded(
+            { groupId: _groupId },
+            { fromBlock: BigInt(this._options.startBlock!), toBlock: "latest" }
+        )
+        const members: string[] = []
+
+        for (const { args } of memberAddedEvents) {
+            const { index, identityCommitment } = args
+            const groupUpdate = groupUpdates.get(index?.toString()!)
+            const member = groupUpdate ? groupUpdate[1].toString() : identityCommitment?.toString()!
+
+            members.push(member)
+        }
+
+        return members
+    }
+
+    async getGroupVerifiedProofs(groupId: string) {
+        checkParameter(groupId, "groupId", "string")
+
+        const _groupId = BigInt(groupId)
+        const [groupCreatedEvent] = await this._contract.getEvents.GroupCreated(
+            { groupId: _groupId },
+            { fromBlock: BigInt(this._options.startBlock!), toBlock: "latest" }
+        )
+
+        if (!groupCreatedEvent) {
+            throw new Error(`Group '${groupId}' not found`)
+        }
+
+        const proofVerifiedEvents = await this._contract.getEvents.ProofVerified(
+            { groupId: _groupId },
+            { fromBlock: BigInt(this._options.startBlock!), toBlock: "latest" }
+        )
+
+        return proofVerifiedEvents.map((event) => ({
+            signal: event.args.signal?.toString() as string,
+            merkleTreeRoot: event.args.merkleTreeRoot?.toString() as string,
+            externalNullifier: event.args.externalNullifier?.toString() as string,
+            nullifierHash: event.args.nullifierHash?.toString() as string
+        }))
     }
 }
