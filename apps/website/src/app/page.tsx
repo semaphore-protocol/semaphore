ce6242dc4828b4ec628908614fc28c5baef50059
cedoor <me@cedoor.dev>
2023-10-20 16:19:11 +0100

    refactor(website): move container component to layout

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -1,20 +1,17 @@
 "use client"
 
 import { Heading, Text, VStack } from "@chakra-ui/react"
-import Container from "../components/Container"
 
 export default function Home() {
     return (
-        <Container>
-            <VStack h="400" justify="center">
-                <Heading as="h2" size="xl">
-                    Anonymous interactions
-                </Heading>
-                <Text align="center">
-                    Using zero knowledge, users can prove their group membership <br />
-                    and send signals such as votes or endorsements without revealing their original identity.
-                </Text>
-            </VStack>
-        </Container>
+        <VStack h="400" justify="center">
+            <Heading as="h2" size="xl">
+                Anonymous interactions
+            </Heading>
+            <Text align="center">
+                Using zero knowledge, users can prove their group membership <br />
+                and send signals such as votes or endorsements without revealing their original identity.
+            </Text>
+        </VStack>
     )
 }
