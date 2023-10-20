026dbdeee0b740c3999d73109b05a6b788d27800
cedoor <me@cedoor.dev>
2023-10-20 13:34:52 +0100

    feat(website): create footer component

--- apps/website/src/components/Container.tsx
+++ apps/website/src/components/Container.tsx
@@ -1,4 +1,5 @@
 import { Container as _Container, ContainerProps } from "@chakra-ui/react"
+import Footer from "./Footer"
 import Navbar from "./Navbar"
 
 export default function Container({ children }: ContainerProps) {
@@ -6,6 +7,7 @@ export default function Container({ children }: ContainerProps) {
         <_Container maxW="1440px" px="20">
             <Navbar />
             {children}
+            <Footer />
         </_Container>
     )
 }
