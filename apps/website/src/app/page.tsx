c3846b2222caea42d2be328b5df5390d6328c155
cedoor <me@cedoor.dev>
2023-10-24 15:47:39 +0100

    feat(website): add hero section
    #371
    
    
    Former-commit-id: 76f3e65765fcbe9fd5b7eb7990e2379fdd747d71

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -1,17 +1,51 @@
 "use client"
 
-import { Heading, Text, VStack } from "@chakra-ui/react"
+import { Box, Button, Heading, HStack, Link, Text, VStack } from "@chakra-ui/react"
 
 export default function Home() {
     return (
-        <VStack h="400" justify="center">
-            <Heading as="h2" size="xl">
-                Anonymous interactions
-            </Heading>
-            <Text align="center">
-                Using zero knowledge, users can prove their group membership <br />
-                and send signals such as votes or endorsements without revealing their original identity.
-            </Text>
-        </VStack>
+        <>
+            <Box
+                zIndex="-1"
+                left="0"
+                w="100%"
+                h="724"
+                pos="absolute"
+                bgImg="url('./section-1.png')"
+                bgSize="100%"
+                bgPos="center"
+                bgRepeat="no-repeat"
+            />
+            <VStack h="724" justify="center" spacing="40">
+                <VStack>
+                    <Heading fontSize="72px" textAlign="center">
+                        Anonymous interactions
+                    </Heading>
+                    <Text fontSize="20px" align="center">
+                        Using zero knowledge, users can prove their group membership and send <br /> signals such as
+                        votes or endorsements without revealing their original identity.
+                    </Text>
+                </VStack>
+
+                <HStack spacing="8">
+                    <Link href="https://demo.semaphore.pse.dev" target="_blank">
+                        <Button size="lg">Try the Demo</Button>
+                    </Link>
+                    <Link
+                        href="https://github.com/semaphore-protocol/semaphore/tree/main/packages/cli#--------semaphore-cli----"
+                        target="_blank"
+                    >
+                        <Button size="lg" variant="outline">
+                            Build with CLI
+                        </Button>
+                    </Link>
+                    <Link href="https://github.com/semaphore-protocol/boilerplate" target="_blank">
+                        <Button size="lg" variant="outline">
+                            Build with Boilerplate
+                        </Button>
+                    </Link>
+                </HStack>
+            </VStack>
+        </>
     )
 }
