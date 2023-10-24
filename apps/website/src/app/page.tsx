3dc3f9c66a3d926b887298aa82f538cfbe82a933
cedoor <me@cedoor.dev>
2023-10-24 16:17:24 +0100

    feat(website): add project section
    #371

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -1,6 +1,9 @@
 "use client"
 
-import { Box, Button, Heading, HStack, Link, Text, VStack } from "@chakra-ui/react"
+import { Box, Button, Heading, HStack, IconButton, Link, Text, VStack } from "@chakra-ui/react"
+import ProjectCard from "../components/ProjectCard"
+import IconArrowLeft from "../icons/IconArrowLeft"
+import IconArrowRight from "../icons/IconArrowRight"
 
 export default function Home() {
     return (
@@ -44,6 +47,59 @@ export default function Home() {
                             Build with Boilerplate
                         </Button>
                     </Link>
+                </HStack>
+            </VStack>
+
+            <VStack py="32" spacing="16">
+                <Heading fontSize="44px" textAlign="center">
+                    Explore projects built with Semaphore
+                </Heading>
+
+                <HStack spacing="8">
+                    <Link href="" target="_blank">
+                        <ProjectCard
+                            title="Project Name"
+                            description="Malesuada facilisi libero, nam eu. Quis pellentesque tortor a elementum ut blandit sed pellentesque arcu."
+                            tags={["Tag", "Tag", "Tag"]}
+                        />
+                    </Link>
+                    <Link href="" target="_blank">
+                        <ProjectCard
+                            title="Project Name"
+                            description="Malesuada facilisi libero, nam eu. Quis pellentesque tortor a elementum ut blandit sed pellentesque arcu."
+                            tags={["Tag", "Tag", "Tag"]}
+                        />
+                    </Link>
+                    <Link href="" target="_blank">
+                        <ProjectCard
+                            title="Project Name"
+                            description="Malesuada facilisi libero, nam eu. Quis pellentesque tortor a elementum ut blandit sed pellentesque arcu."
+                            tags={["Tag", "Tag", "Tag"]}
+                        />
+                    </Link>
+                </HStack>
+
+                <HStack w="100%">
+                    <Box flex="1" />
+
+                    <HStack flex="1" justify="center">
+                        <IconButton variant="link" aria-label="Arrow left" icon={<IconArrowLeft />} />
+                        <IconButton variant="link" aria-label="Arrow right" icon={<IconArrowRight />} />
+                    </HStack>
+
+                    <HStack flex="1" justify="right" fontSize="12px">
+                        <Link
+                            href=""
+                            target="_blank"
+                            textTransform="uppercase"
+                            textDecoration="underline"
+                            _hover={{
+                                textDecoration: "underline"
+                            }}
+                        >
+                            View more
+                        </Link>
+                    </HStack>
                 </HStack>
             </VStack>
         </>
