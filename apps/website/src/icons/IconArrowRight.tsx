e3430d79e9580bdb3ba1aa787f7fb0ce34da6e4b
vplasencia <v.pcalana@gmail.com>
2023-10-30 11:18:52 +0100

    feat(website): add build page
    Former-commit-id: 0c06bab6195808656526d27809316a81d623ca60

---
+++ apps/website/src/icons/IconArrowRight.tsx
@@ -0,0 +1,13 @@
+import { Icon, IconProps } from "@chakra-ui/react"
+import React from "react"
+
+export default function IconArrowRight(props: IconProps): JSX.Element {
+    return (
+        <Icon viewBox="00 0 14 14" {...props}>
+            <path
+                d="M10.4765 6.16664L0.333172 6.16664L0.333172 7.83331L10.4765 7.83331L6.0065 12.3033L7.18484 13.4816L13.6665 6.99998L7.18484 0.51831L6.0065 1.69664L10.4765 6.16664Z"
+                fill="currentColor"
+            />
+        </Icon>
+    )
+}
