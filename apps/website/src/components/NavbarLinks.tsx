b71ae4c5d95a8129ae9659b98ae23f57e64f3f03
cedoor <me@cedoor.dev>
2023-11-07 12:34:02 +0000

    refactor(website): make navbar responsive

---
+++ apps/website/src/components/NavbarLinks.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { HStack, Heading, Link } from "@chakra-ui/react"
+import NextLink from "next/link"
+import { usePathname } from "next/navigation"
+import IconArrowUpRight from "../icons/IconArrowUpRight"
+
+export default function NavbarLinks() {
+    const pathname = usePathname()
+
+    return (
+        <>
+            <Link
+                as={NextLink}
+                href="/projects"
+                variant="navlink"
+                borderBottomColor={pathname === "/projects" ? "ceruleanBlue" : "transparent"}
+            >
+                <Heading fontSize="18px" fontWeight="normal">
+                    Projects
+                </Heading>
+            </Link>
+            <Link
+                as={NextLink}
+                href="/learn"
+                variant="navlink"
+                borderBottomColor={pathname === "/learn" ? "ceruleanBlue" : "transparent"}
+            >
+                <Heading fontSize="18px" fontWeight="normal">
+                    Learn
+                </Heading>
+            </Link>
+            <Link
+                as={NextLink}
+                href="/build"
+                variant="navlink"
+                borderBottomColor={pathname === "/build" ? "ceruleanBlue" : "transparent"}
+            >
+                <Heading fontSize="18px" fontWeight="normal">
+                    Build
+                </Heading>
+            </Link>
+            <Link href="https://docs.semaphore.pse.dev" variant="navlink" isExternal>
+                <HStack spacing="3">
+                    <Heading fontSize="18px" fontWeight="normal">
+                        Documentation
+                    </Heading>
+                    <IconArrowUpRight width="10px" mb={1} />
+                </HStack>
+            </Link>
+            <Link href="https://github.com/semaphore-protocol" variant="navlink" isExternal>
+                <HStack spacing="3">
+                    <Heading fontSize="18px" fontWeight="normal">
+                        Github
+                    </Heading>
+                    <IconArrowUpRight width="10px" mb={1} />
+                </HStack>
+            </Link>
+        </>
+    )
+}
