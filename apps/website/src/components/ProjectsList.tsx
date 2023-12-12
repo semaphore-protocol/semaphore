de6a55ffd1e9ccdccc30d69bc1318ed9c4f4985f
Jeeiii <giacomo.corrias7@gmail.com>
2023-12-12 12:40:03 +0100

    fix: missing double filter logic on created by and category
    Former-commit-id: a6fde2fdedb3ad70970e643b4a212364ea77c6fa

--- apps/website/src/components/ProjectsList.tsx
+++ apps/website/src/components/ProjectsList.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Button, Grid, GridItem, HStack, IconButton, Text, VStack } from "@chakra-ui/react"
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import ProjectCard from "../components/ProjectCard"
 import allProjects from "../data/projects.json"
 import IconChevronLeft from "../icons/IconChevronLeft"
@@ -12,18 +12,34 @@ import { chunkArray } from "../utils/chunkArray"
 import { getProjectCategories } from "../utils/getProjectCategories"
 
 export default function ProjectsList(props: any) {
-    const [projects] = useState<(typeof allProjects)[]>(chunkArray(allProjects))
+    const [projects, setProjects] = useState<(typeof allProjects)[]>(chunkArray(allProjects))
     const [index, setIndex] = useState<number>(0)
+    const [selectedCategories, setSelectedCategories] = useState<string[]>([])
     const [onlyPSE, setOnlyPSE] = useState<boolean | null>(null)
-    const [selectedCategories, setSelectedCategories] = useState<Array<string>>([])
 
-    const toggleCategory = (category: any) => {
-        if (selectedCategories.includes(category)) {
-            setSelectedCategories(selectedCategories.filter((c) => c !== category))
-        } else {
-            setSelectedCategories([...selectedCategories, category])
+    const filterProjects = useCallback(() => {
+        let filteredProjects = allProjects
+
+        if (selectedCategories.length > 0) {
+            filteredProjects = filteredProjects.filter((project) =>
+                selectedCategories.every((category) => project.categories.includes(category))
+            )
         }
-    }
+
+        if (onlyPSE === true) {
+            filteredProjects = filteredProjects.filter((project) => project.pse)
+        } else if (onlyPSE === false) {
+            filteredProjects = filteredProjects.filter((project) => !project.pse)
+        }
+
+        filteredProjects = filteredProjects.sort((a, b) => a.name.localeCompare(b.name))
+
+        setProjects(chunkArray(filteredProjects))
+    }, [selectedCategories, onlyPSE])
+
+    useEffect(() => {
+        filterProjects()
+    }, [selectedCategories, onlyPSE])
 
     return (
         <VStack {...props}>
@@ -52,37 +68,37 @@ export default function ProjectsList(props: any) {
                 </HStack>
             </VStack>
 
-            <VStack align="left" spacing="6">
-                <Text fontSize="20">Category</Text>
+            <HStack spacing="3" flexWrap="wrap">
+                {getProjectCategories(allProjects).map((category) => (
+                    <Button
+                        key={category}
+                        size="sm"
+                        variant={selectedCategories.includes(category) ? "solid" : "outline"}
+                        colorScheme={selectedCategories.includes(category) ? "primary" : "inherit"}
+                        onClick={() => {
+                            const newCategories = selectedCategories.includes(category)
+                                ? selectedCategories.filter((c) => c !== category)
+                                : [...selectedCategories, category]
 
-                <HStack spacing="3" flexWrap="wrap">
-                    {getProjectCategories(allProjects).map((category) => (
-                        <Button
-                            key={category}
-                            size="sm"
-                            variant={selectedCategories.includes(category) ? "solid" : "outline"}
-                            colorScheme={selectedCategories.includes(category) ? "primary" : "inherit"}
-                            onClick={() => toggleCategory(category)}
-                        >
-                            {category}
-                        </Button>
-                    ))}
-                </HStack>
-            </VStack>
+                            setSelectedCategories(newCategories)
+                        }}
+                    >
+                        {category}
+                    </Button>
+                ))}
+            </HStack>
 
             <Grid templateColumns={{ base: "1fr", lg: "repeat(2, 1fr)", "2xl": "repeat(3, 1fr)" }} gap={6}>
-                {projects[index]
-                    .filter((project) => selectedCategories.every((category) => project.categories.includes(category)))
-                    .map((project) => (
-                        <GridItem key={project.name}>
-                            <ProjectCard
-                                title={project.name}
-                                description={project.tagline}
-                                categories={project.categories}
-                                url={project.links.website || project.links.github}
-                            />
-                        </GridItem>
-                    ))}
+                {projects[index].map((project) => (
+                    <GridItem key={project.name}>
+                        <ProjectCard
+                            title={project.name}
+                            description={project.tagline}
+                            categories={project.categories}
+                            url={project.links.website || project.links.github}
+                        />
+                    </GridItem>
+                ))}
             </Grid>
 
             {projects.length > 1 && (
