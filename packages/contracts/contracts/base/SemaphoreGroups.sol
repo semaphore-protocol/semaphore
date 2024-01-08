1aee724187e92ea64011b43d832a8ad100849de5
cedoor <me@cedoor.dev>
2024-01-08 17:30:05 +0000

    chore(contracts): update solidity and hardhat versions

--- packages/contracts/contracts/base/SemaphoreGroups.sol
+++ packages/contracts/contracts/base/SemaphoreGroups.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity 0.8.19;
 
 import {ISemaphoreGroups} from "../interfaces/ISemaphoreGroups.sol";
 import {InternalLeanIMT, LeanIMTData} from "@zk-kit/imt.sol/internal/InternalLeanIMT.sol";
