59b8fd30b653384bc738e02dd13bd9b8032b92aa
cedoor <me@cedoor.dev>
2024-01-08 17:30:05 +0000

    chore(contracts): update solidity and hardhat versions
    Former-commit-id: 94453ad50e6df33265ae2a237a067c5e046cc62b

--- packages/contracts/tasks/accounts.ts
+++ packages/contracts/tasks/accounts.ts
@@ -1,10 +1,10 @@
-import { Signer } from "@ethersproject/abstract-signer"
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers"
 import { task, types } from "hardhat/config"
 
 task("accounts", "Prints the list of accounts")
     .addOptionalParam<boolean>("logs", "Print the logs", true, types.boolean)
     .setAction(async ({ logs }, { ethers }) => {
-        const accounts: Signer[] = await ethers.getSigners()
+        const accounts: HardhatEthersSigner[] = await ethers.getSigners()
 
         if (logs) {
             for (const account of accounts) {
