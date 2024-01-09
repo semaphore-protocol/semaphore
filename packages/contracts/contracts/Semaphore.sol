205fff71479f9cb2693a44893f67e4d0b64174ff
cedoor <me@cedoor.dev>
2024-01-09 13:52:04 +0000

    chore(contracts): update solidity version
    Former-commit-id: 9bb72aa7d4dd55f5068ece2d372f49d0c81a008f

--- packages/contracts/contracts/Semaphore.sol
+++ packages/contracts/contracts/Semaphore.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.23;
 
 import {ISemaphore} from "./interfaces/ISemaphore.sol";
 import {ISemaphoreVerifier} from "./interfaces/ISemaphoreVerifier.sol";
