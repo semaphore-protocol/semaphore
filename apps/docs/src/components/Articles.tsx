fb7b683c9e4e43ab61a071cfed0b61c58ba4b009
cedoor <me@cedoor.dev>
2024-01-29 18:06:56 +0000

    docs(docs): fetch list of articles/videos
    Former-commit-id: f742023c2f9bca7cc6a7a041b11ebdd4eb6ec3ae

---
+++ apps/docs/src/components/Articles.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react"
+
+export default function Articles() {
+    const [articles, setArticles] = useState<any[]>([])
+
+    useEffect(() => {
+        fetch("https://raw.githubusercontent.com/semaphore-protocol/semaphore/main/apps/website/src/data/articles.json")
+            .then((response) => response.json())
+            .then(setArticles)
+    }, [])
+
+    return (
+        <ul>
+            {articles.map((article) => (
+                <li key={article.url + article.title}>
+                    <a href={article.url} target="_blank" rel="noreferrer">
+                        {article.title}
+                    </a>{" "}
+                    - {article.authors.join(", ")} (<i>{article.date}</i>)
+                </li>
+            ))}
+        </ul>
+    )
+}
