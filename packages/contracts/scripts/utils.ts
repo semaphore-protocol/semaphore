c610213d5464309a99c935b77893ac2c79c91716
cedoor <me@cedoor.dev>
2024-01-31 14:10:45 +0000

    refactor(contracts): update scripts to save deployed contract addresses

--- packages/contracts/scripts/utils.ts
+++ packages/contracts/scripts/utils.ts
@@ -1,15 +1,62 @@
-import { readFileSync } from "fs"
+import { readFileSync, writeFileSync } from "fs"
+
+export type NetworkDeployedContracts = {
+    name: "Semaphore" | "SemaphoreVerifier" | "PoseidonT3"
+    address: string
+}[]
 
 export type DeployedContracts = {
-    Poseidon: string
-    Semaphore: string
-    Verifier: string
+    network: string
+    contracts: NetworkDeployedContracts
+}[]
+
+const supportedNetworks = ["sepolia", "arbitrum", "mumbai", "optimism-sepolia", "arbitrum-sepolia"]
+
+export function getDeployedContracts(): DeployedContracts {
+    return JSON.parse(readFileSync(`./deployed-contracts.json`, "utf8"))
 }
 
-export function getDeployedContracts(network: string | undefined): DeployedContracts | null {
-    try {
-        return JSON.parse(readFileSync(`./deployed-contracts/${network}.json`, "utf8"))
-    } catch (error) {
-        return null
+export function getDeployedContractsByNetwork(network: string): NetworkDeployedContracts {
+    const deployedContracts = getDeployedContracts()
+    const networkDeployedContracts = deployedContracts.find((n) => n.network === network)
+
+    if (!networkDeployedContracts) {
+        throw Error(`Network '${network}' is not supported`)
+    }
+
+    return networkDeployedContracts.contracts
+}
+
+export function getDeployedContractAddress(network: string, contractName: string): string {
+    const contracts = getDeployedContractsByNetwork(network)
+    const semaphoreAddress = contracts.find((contract) => contract.name === contractName)
+
+    if (!semaphoreAddress) {
+        throw Error(`Contract with name '${contractName}' does not exist`)
+    }
+
+    return semaphoreAddress.address
+}
+
+export function saveDeployedContracts(contracts: NetworkDeployedContracts, network?: string) {
+    if (network && supportedNetworks.includes(network)) {
+        const deployedContracts = getDeployedContracts() as DeployedContracts
+
+        for (let i = 0; i < deployedContracts.length; i += 1) {
+            if (deployedContracts[i].network === network) {
+                deployedContracts[i].contracts = contracts
+
+                writeFileSync(`./deployed-contracts.json`, JSON.stringify(deployedContracts, null, 4))
+
+                return
+            }
+        }
+
+        deployedContracts.push({
+            network,
+            contracts
+        })
+
+        writeFileSync(`./deployed-contracts.json`, JSON.stringify(deployedContracts, null, 4))
     }
 }
