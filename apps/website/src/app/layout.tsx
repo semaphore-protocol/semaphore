3a1f0782fcdbb446c8806370f8e8623ec0e68706
cedoor <me@cedoor.dev>
2024-02-08 16:39:26 +0000

    chore: add matomo script for cookies

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Box, Container } from "@chakra-ui/react"
 import type { Metadata } from "next"
+import Script from "next/script"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import Providers from "./providers"
@@ -37,6 +38,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     </Container>
                 </Providers>
             </body>
+
+            <Script id="matomo-tracking" strategy="afterInteractive">
+                {`
+                    var _paq = window._paq = window._paq || [];
+                    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+                    _paq.push(['trackPageView']);
+                    _paq.push(['enableLinkTracking']);
+                    (function() {
+                        var u="https://psedev.matomo.cloud/";
+                        _paq.push(['setTrackerUrl', u+'matomo.php']);
+                        _paq.push(['setSiteId', '10']);
+                        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                        g.async=true; g.src='//cdn.matomo.cloud/psedev.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+                    })();
+                `}
+            </Script>
         </html>
     )
 }
