1af0b407aadf9df994165f4a893811ceeb4b8867
cedoor <me@cedoor.dev>
2024-02-12 12:18:10 +0000

    feat(group): add export/import methods

--- packages/group/tests/index.test.ts
+++ packages/group/tests/index.test.ts
@@ -91,4 +91,34 @@ describe("Group", () => {
             expect(proof.leaf).toBe("1")
         })
     })
+
+    describe("# export", () => {
+        it("Should export a group", () => {
+            const group = new Group([1, 2, 3])
+
+            const exportedGroup = group.export()
+
+            console.log(exportedGroup)
+
+            expect(typeof exportedGroup).toBe("string")
+            expect(JSON.parse(exportedGroup)).toHaveLength(3)
+            expect(JSON.parse(exportedGroup)[0]).toHaveLength(3)
+        })
+    })
+
+    describe("# import", () => {
+        it("Should import a group", () => {
+            const group1 = new Group([1, 2, 3])
+            const exportedGroup = group1.export()
+
+            const group2 = Group.import(exportedGroup)
+
+            group1.addMember(4)
+            group2.addMember(4)
+
+            expect(group2.depth).toBe(group1.depth)
+            expect(group2.size).toBe(group1.size)
+            expect(group2.root).toBe(group1.root)
+        })
+    })
 })
