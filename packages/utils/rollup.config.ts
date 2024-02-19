785f955b2a3d02bfbf82617f7517c06f6468db10
cedoor <me@cedoor.dev>
2024-02-19 12:38:08 +0000

    feat(utils): create new utils package

---
+++ packages/utils/rollup.config.ts
@@ -0,0 +1,30 @@
+import * as fs from "fs"
+import cleanup from "rollup-plugin-cleanup"
+import typescript from "rollup-plugin-typescript2"
+
+const pkg = JSON.parse(fs.readFileSync("./package.json", "utf-8"))
+const banner = `/**
+ * @module ${pkg.name}
+ * @version ${pkg.version}
+ * @file ${pkg.description}
+ * @copyright Ethereum Foundation 2024
+ * @license ${pkg.license}
+ * @see [Github]{@link ${pkg.homepage}}
+*/`
+
+export default {
+    input: "src/index.ts",
+    output: [
+        { file: pkg.exports["."].require, format: "cjs", banner, exports: "auto" },
+        { file: pkg.exports["."].default, format: "es", banner },
+        { dir: "./dist/lib.commonjs", format: "cjs", banner, preserveModules: true },
+        { dir: "./dist/lib.esm", format: "es", banner, preserveModules: true }
+    ],
+    plugins: [
+        typescript({
+            tsconfig: "./build.tsconfig.json",
+            useTsconfigDeclarationDir: true
+        }),
+        cleanup({ comments: "jsdoc" })
+    ]
+}
