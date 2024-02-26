d28448624adedcbf08230701b0da5f532636e319
vplasencia <v.pcalana@gmail.com>
2024-02-26 12:38:01 +0100

    refactor(proof): integrate pack unpack groth16 proof

--- packages/proof/src/index.ts
+++ packages/proof/src/index.ts
@@ -1,8 +1,7 @@
+import { packGroth16Proof, unpackGroth16Proof } from "@zk-kit/utils"
 import generateProof from "./generate-proof"
 import getSnarkArtifacts from "./get-snark-artifacts.node"
-import packPoints from "./pack-points"
-import unpackPoints from "./unpack-points"
 import verifyProof from "./verify-proof"
 
 export * from "./types"
-export { generateProof, getSnarkArtifacts, packPoints, unpackPoints, verifyProof }
+export { generateProof, getSnarkArtifacts, packGroth16Proof, unpackGroth16Proof, verifyProof }
