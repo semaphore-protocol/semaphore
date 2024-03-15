9e5f8351cca221c7628acd04a26d34a72c6f19d0
cedoor <me@cedoor.dev>
2024-03-15 23:08:47 +0000

    fix(utils): add missing exports to index.ts
    re #642
    
    
    Former-commit-id: 0987dee8db4bcbda637d1938abd5f51f13d78591

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,5 +1,6 @@
 import * as constants from "./constants"
+import decodeMessage from "./decode-message"
 import supportedNetworks from "./supported-networks"
 
 export * from "./types"
-export { constants, supportedNetworks }
+export { constants, decodeMessage, supportedNetworks }
