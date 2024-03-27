1942afe766f7579057818bf13e4009c2d56fb496
cedoor <me@cedoor.dev>
2024-03-27 12:38:09 +0000

    refactor(docs): use utils function to get explorer url
    re #599
    
    
    Former-commit-id: e05d89ce63d3e7a66de7d0f4823ec261fb5a21b0

--- apps/docs/src/components/DeployedContracts.tsx
+++ apps/docs/src/components/DeployedContracts.tsx
@@ -1,25 +1,8 @@
-import { deployedContracts } from "@semaphore-protocol/utils"
+import { deployedContracts, supportedNetworks } from "@semaphore-protocol/utils"
 import Heading from "@theme/Heading"
 
 function capitalizeFirstLetter(s: string): string {
     return s.charAt(0).toUpperCase() + s.slice(1)
-}
-
-function getEtherscanLink(network: string): string {
-    switch (network) {
-        case "sepolia":
-            return "https://sepolia.etherscan.io/address/"
-        case "mumbai":
-            return "https://mumbai.polygonscan.com/address/"
-        case "arbitrum":
-            return "https://arbiscan.io/address/"
-        case "arbitrum-sepolia":
-            return "https://sepolia.arbiscan.io/address/"
-        case "optimism-sepolia":
-            return "https://sepolia-optimism.etherscan.io/address/"
-        default:
-            return ""
-    }
 }
 
 export default function DeployedContracts() {
@@ -32,7 +15,11 @@ export default function DeployedContracts() {
                         {contracts.map(({ name, address }) => (
                             <li key={address}>
                                 {name}:{" "}
-                                <a href={getEtherscanLink(network) + address} target="_blank" rel="noreferrer">
+                                <a
+                                    href={`${supportedNetworks[network].explorer}/address/${address}`}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
                                     {address}
                                 </a>
                             </li>
