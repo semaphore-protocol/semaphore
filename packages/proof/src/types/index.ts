69089259f8a12e4e46f3779471aaf988e8aa9a8b
sripwoud <me@sripwoud.xyz>
2024-04-24 12:19:57 +0200

    refactor(proof): use `maybeGetSemaphoreSnarkArtifacts` from `@zk-kit/utils`
    Delete logic related to fetching snark artifacts (wasm and zkey files) that was moved to
    `@zk-kit/utils`

--- packages/proof/src/types/index.ts
+++ packages/proof/src/types/index.ts
@@ -1,11 +1,6 @@
 import type { NumericString } from "snarkjs"
 import type { PackedGroth16Proof } from "@zk-kit/utils"
 
-export type SnarkArtifacts = {
-    wasmFilePath: string
-    zkeyFilePath: string
-}
-
 export type SemaphoreProof = {
     merkleTreeDepth: number
     merkleTreeRoot: NumericString
