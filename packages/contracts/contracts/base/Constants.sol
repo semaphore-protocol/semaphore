7f3853b3810f641b0bb40beabc44b5b9f5773b56
Vivian Plasencia <v.pcalana@gmail.com>
2024-05-12 14:25:47 +0200

    feat: support more tree depth
    Now Semaphore v4 will support tree depth from 2 to 32.
    
    re #778

--- packages/contracts/contracts/base/Constants.sol
+++ packages/contracts/contracts/base/Constants.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.23;
 
 /// @dev Minimum supported tree depth.
-uint8 constant MIN_DEPTH = 1;
+uint8 constant MIN_DEPTH = 2;
 
 /// @dev Maximum supported tree depth.
-uint8 constant MAX_DEPTH = 12;
+uint8 constant MAX_DEPTH = 32;
--- packages/utils/src/constants.ts
+++ packages/utils/src/constants.ts
@@ -1,5 +1,5 @@
-// TODO: Support all tree depths after trusted-setup. MAX_DEPTH would be 32.
+// Minimum supported tree depth.
+export const MIN_DEPTH = 2
 
-export const MIN_DEPTH = 1
-
-export const MAX_DEPTH = 12
+// Maximum supported tree depth.
+export const MAX_DEPTH = 32
