cada7fa9ac7f17d8a4e24902292d864d09511ef6
Vivian Plasencia <v.pcalana@gmail.com>
2024-05-15 19:26:53 +0200

    chore: support tree depth 1
    re #778

--- packages/contracts/contracts/base/Constants.sol
+++ packages/contracts/contracts/base/Constants.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.23;
 
 /// @dev Minimum supported tree depth.
-uint8 constant MIN_DEPTH = 2;
+uint8 constant MIN_DEPTH = 1;
 
 /// @dev Maximum supported tree depth.
 uint8 constant MAX_DEPTH = 32;
--- packages/utils/src/constants.ts
+++ packages/utils/src/constants.ts
@@ -1,5 +1,5 @@
 // Minimum supported tree depth.
-export const MIN_DEPTH = 2
+export const MIN_DEPTH = 1
 
 // Maximum supported tree depth.
 export const MAX_DEPTH = 32
