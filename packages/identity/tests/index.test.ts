e03a8f10d86c3e8021d2b05b16c086b2dab39374
cedoor <me@cedoor.dev>
2024-06-17 10:28:47 +0100

    test(identity): add more tests
    re #799

--- packages/identity/tests/index.test.ts
+++ packages/identity/tests/index.test.ts
@@ -1,37 +1,36 @@
+import { derivePublicKey, deriveSecretScalar } from "@zk-kit/eddsa-poseidon"
+import { poseidon2 } from "poseidon-lite"
 import { Identity } from "../src"
 
 describe("Identity", () => {
     const privateKeyText = "secret"
-    const privateKeyBuffer = Buffer.from(privateKeyText)
+    const privateKeyBuffer = Buffer.from("another secret")
 
     describe("# Identity", () => {
         it("Should create an identity with a random secret (private key)", () => {
             const identity = new Identity()
+            const privateKey = Buffer.from(identity.privateKey)
 
-            expect(typeof identity.privateKey).toBe("object")
             expect(identity.privateKey).toHaveLength(32)
-            expect(typeof identity.secretScalar).toBe("bigint")
-            expect(identity.publicKey).toHaveLength(2)
-            expect(typeof identity.commitment).toBe("bigint")
+            expect(identity.secretScalar).toBe(deriveSecretScalar(privateKey))
+            expect(identity.publicKey).toStrictEqual(derivePublicKey(privateKey))
+            expect(identity.commitment).toBe(poseidon2(identity.publicKey))
         })
 
         it("Should create a deterministic identity from a secret text (private key)", () => {
             const identity = new Identity(privateKeyText)
 
-            expect(typeof identity.privateKey).toBe("string")
-            expect(typeof identity.secretScalar).toBe("bigint")
-            expect(identity.publicKey).toHaveLength(2)
-            expect(typeof identity.commitment).toBe("bigint")
+            expect(identity.secretScalar).toBe(deriveSecretScalar(privateKeyText))
+            expect(identity.publicKey).toStrictEqual(derivePublicKey(privateKeyText))
+            expect(identity.commitment).toBe(poseidon2(identity.publicKey))
         })
 
         it("Should create a deterministic identity from a secret buffer (private key)", () => {
             const identity = new Identity(privateKeyBuffer)
 
-            expect(typeof identity.privateKey).toBe("object")
-            expect(identity.privateKey).toHaveLength(6)
-            expect(typeof identity.secretScalar).toBe("bigint")
-            expect(identity.publicKey).toHaveLength(2)
-            expect(typeof identity.commitment).toBe("bigint")
+            expect(identity.secretScalar).toBe(deriveSecretScalar(privateKeyBuffer))
+            expect(identity.publicKey).toStrictEqual(derivePublicKey(privateKeyBuffer))
+            expect(identity.commitment).toBe(poseidon2(identity.publicKey))
         })
 
         it("Should create the same identity if the private key is the same", () => {
@@ -39,6 +38,9 @@ describe("Identity", () => {
 
             const identity2 = new Identity(identity.privateKey)
 
+            expect(identity.privateKey).toStrictEqual(identity2.privateKey)
+            expect(identity.secretScalar).toBe(identity2.secretScalar)
+            expect(identity.publicKey).toStrictEqual(identity2.publicKey)
             expect(identity.commitment).toBe(identity2.commitment)
         })
 
@@ -60,7 +62,7 @@ describe("Identity", () => {
         })
 
         it("Should export an identity where the private key is text", () => {
-            const identity = new Identity(privateKeyText)
+            const identity = new Identity(privateKeyBuffer)
 
             const privateKey = identity.export()
 
@@ -70,12 +72,39 @@ describe("Identity", () => {
     })
 
     describe("# import", () => {
-        it("Should import an identity", () => {
+        it("Should import an identity with a private key of buffer type", () => {
             const identity = new Identity(privateKeyBuffer)
             const privateKey = identity.export()
 
             const identity2 = Identity.import(privateKey)
 
+            expect(identity2.privateKey).toStrictEqual(identity.privateKey)
+            expect(identity2.secretScalar).toBe(identity.secretScalar)
+            expect(identity2.publicKey).toStrictEqual(identity.publicKey)
+            expect(identity2.commitment).toBe(identity.commitment)
+        })
+
+        it("Should import an identity with a private key of text type", () => {
+            const identity = new Identity(privateKeyText)
+            const privateKey = identity.export()
+
+            const identity2 = Identity.import(privateKey)
+
+            expect(identity2.privateKey).toStrictEqual(Buffer.from(identity.privateKey))
+            expect(identity2.secretScalar).toBe(identity.secretScalar)
+            expect(identity2.publicKey).toStrictEqual(identity.publicKey)
+            expect(identity2.commitment).toBe(identity.commitment)
+        })
+
+        it("Should import an identity generated from a random private key", () => {
+            const identity = new Identity()
+            const privateKey = identity.export()
+
+            const identity2 = Identity.import(privateKey)
+
+            expect(identity2.privateKey).toStrictEqual(Buffer.from(identity.privateKey))
+            expect(identity2.secretScalar).toBe(identity.secretScalar)
+            expect(identity2.publicKey).toStrictEqual(identity.publicKey)
             expect(identity2.commitment).toBe(identity.commitment)
         })
     })
