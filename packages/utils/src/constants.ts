d813b364132d1e0f319de5e7cdc75efc36f87cd1
Dozie2001 <davidchidozie402@gmail.com>
2024-07-05 22:17:29 +0100

    refactor(utils): allows packages to import abi from @semaphore-protocol/utils
    re #817

--- packages/utils/src/constants.ts
+++ packages/utils/src/constants.ts
@@ -1,5 +1,10 @@
+import abi from "./semaphore-abi.json"
+
 // Minimum supported tree depth.
 export const MIN_DEPTH = 1
 
 // Maximum supported tree depth.
 export const MAX_DEPTH = 32
+
+// Semaphore ABI
+export const SemaphoreABI = abi.abi
