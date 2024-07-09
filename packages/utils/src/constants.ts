dc5e6b1221ce1dd9c27449ada3c9a36311cab9ee
Dozie2001 <davidchidozie402@gmail.com>
2024-07-09 16:15:10 +0100

    chore(utils): changed ab to _interface and update to package.json

--- packages/utils/src/constants.ts
+++ packages/utils/src/constants.ts
@@ -1,4 +1,4 @@
-import abi from "./semaphore-interface.json"
+import _interface from "./semaphore-interface.json"
 
 // Minimum supported tree depth.
 export const MIN_DEPTH = 1
@@ -6,5 +6,5 @@ export const MIN_DEPTH = 1
 // Maximum supported tree depth.
 export const MAX_DEPTH = 32
 
-export const SemaphoreABI = abi.abi
-export const SemaphoreBytecode = abi.bytecode
+export const SemaphoreABI = _interface.abi
+export const SemaphoreBytecode = _interface.bytecode
