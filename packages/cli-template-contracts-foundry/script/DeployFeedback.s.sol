56ae53b70a5dc39ab8f658722f01a9a69e3b76e7
csiejimmyliu <91661606+csiejimmyliu@users.noreply.github.com>
2024-09-05 19:47:15 +0800

    modify declaration of semaphore and verifier

--- packages/cli-template-contracts-foundry/script/DeployFeedback.s.sol
+++ packages/cli-template-contracts-foundry/script/DeployFeedback.s.sol
@@ -10,26 +10,18 @@ import "forge-std/Script.sol";
 contract DeployFeedback is Script {
     function run() external returns (address, address) {
 
-        SemaphoreVerifier semaphoreVerifier;
-        address semaphoreVerifierAddress;
-        ISemaphoreVerifier IsemaphoreVerifier;
-        Semaphore semaphore;
-        address semaphoreAddress;
-        Feedback feedback;
-
         vm.startBroadcast();
-        semaphoreVerifier = new SemaphoreVerifier();
-        semaphoreVerifierAddress = address(semaphoreVerifier);
-        IsemaphoreVerifier = ISemaphoreVerifier(semaphoreVerifierAddress);
 
-        semaphore = new Semaphore(IsemaphoreVerifier);
-        semaphoreAddress = address(semaphore);
+        SemaphoreVerifier semaphoreVerifier = new SemaphoreVerifier();
+        ISemaphoreVerifier IsemaphoreVerifier=ISemaphoreVerifier(address(semaphoreVerifier));
 
-        feedback = new Feedback(semaphoreAddress);
+        Semaphore semaphore=new Semaphore(IsemaphoreVerifier);
+        Feedback feedback=new Feedback(address(semaphore));
+
         vm.stopBroadcast();
 
         console.log("Feedback contract has been deployed to:", address(feedback));
 
-        return (address(feedback), semaphoreAddress);
+        return (address(feedback),  address(semaphore));
     }
 }
