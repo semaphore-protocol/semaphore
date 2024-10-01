fdbadf5b71b6f11d9fd237a6a55b1a809af9954f
Cedoor <me@cedoor.dev>
2024-10-01 11:31:29 +0100

    Chore/mainnet deployments (#862)
    * chore(contracts): deploy contracts on arbitrum
    
    re #859
    
    * chore(contracts): deploy contracts on polygon
    
    re #859
    
    * chore(contracts): deploy contracts on optimism
    
    re #859

--- packages/utils/src/networks/supported-networks.ts
+++ packages/utils/src/networks/supported-networks.ts
@@ -11,16 +11,34 @@ export default {
         chainId: 421614,
         explorer: "https://sepolia.arbiscan.io"
     },
+    arbitrum: {
+        name: "Arbitrum",
+        url: "https://arb1.arbitrum.io/rpc",
+        chainId: 42161,
+        explorer: "https://arbiscan.io"
+    },
     "optimism-sepolia": {
         name: "Optimism Sepolia",
         url: "https://sepolia.optimism.io",
         chainId: 11155420,
         explorer: "https://sepolia-optimism.etherscan.io"
     },
+    optimism: {
+        name: "Optimism",
+        url: "https://mainnet.optimism.io ",
+        chainId: 10,
+        explorer: "https://optimistic.etherscan.io"
+    },
     "matic-amoy": {
         name: "Matic Amoy",
         url: "https://rpc-amoy.polygon.technology",
         chainId: 80002,
         explorer: "https://amoy.polygonscan.com"
+    },
+    matic: {
+        name: "Matic",
+        url: "https://polygon-rpc.com",
+        chainId: 137,
+        explorer: "https://polygonscan.com"
     }
 }
