87709224a3c06a9c0ab7f2119915bb80758c8eb7
bigbear <155267841+aso20455@users.noreply.github.com>
2025-10-22 12:14:46 +0300

    docs: fix incorrect default value in chunkArray JSDoc (#1038)

--- apps/website/src/utils/chunkArray.ts
+++ apps/website/src/utils/chunkArray.ts
@@ -2,7 +2,7 @@
  * This function takes an array and divides it into smaller arrays,
  * or "chunks", each containing a specified number of elements.
  * @param array The array to be split into chunks.
- * @param size The size of each chunk. The default size is 10.
+ * @param size The size of each chunk. The default size is 15.
  * @returns An array containing smaller subarrays (chunks), each with a length defined by the size.
  */
 export function chunkArray(array: any[], size = 15): any[] {
